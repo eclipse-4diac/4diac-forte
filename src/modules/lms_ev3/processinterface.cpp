a5cf20f6329e8fa2655f6b0ab010bf2d3e19837a
Jose Cabral <cabral@fortiss.org>
2016-02-23 15:25:57 +0100

    [488466] Change to the correct path of led and sensors.
    Some path were different to the current version of ev3Dev.
    First tests applications were loaded and successfully
    tested in the Ev3.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?=488466
    Signed-off-by: Jose Cabral <cabral@fortiss.org>

--- src/modules/lms_ev3/processinterface.cpp
+++ src/modules/lms_ev3/processinterface.cpp
@@ -32,11 +32,11 @@ bool CLMSEV3ProcessInterface::initialise(bool paInput){
 
   //TODO check length of vector
   if(paInput){
-    if(scmSensorID == paramsList.front().substr(0,scmSensorID.length())){
-      retVal = setupSensor(paramsList.front());
+    if(scmSensorID == paramsList.front()){ // led parameter format = Sensor.in[X]
+      retVal = setupSensor(paramsList[1]);
     }
   }else{
-    if(scmLEDId == paramsList.front()){
+    if(scmLEDId == paramsList.front()){   // led parameter format = led.[red | green].[right | left]
       retVal = setupLEDOutput(paramsList);
     }
   }
@@ -54,12 +54,15 @@ bool CLMSEV3ProcessInterface::deinitialise(){
 bool CLMSEV3ProcessInterface::readPin(){
   bool retVal = false;
   if(mFile.is_open()){
-    char binData = 0;
+    char binData[10];
+    std::string binDataString;
     mFile.clear();
     mFile.seekg(0, std::ios::beg);
-    mFile.read(&binData, 1);
+    mFile.read(binData, 9);
+    binData[10] = '\0';
+    binDataString.assign(binData);
     //TODO check if reading worked
-    IN_X() = ('0' != binData) ? true : false;
+    IN_X() = (binDataString.substr(0, 4) == "2550") ? true : false; //2250 is the max value for ultrasonic sensor. Here just for test
     retVal = true;
   }
   return retVal;
@@ -90,11 +93,9 @@ std::vector<std::string> CLMSEV3ProcessInterface::generateParameterList(){
 bool CLMSEV3ProcessInterface::setupLEDOutput(const std::vector<std::string> &paParamList){
   bool retVal = false;
 
-  std::string sysFileName("/sys/class/leds/ev3-" + paParamList[2]);
+  std::string sysFileName("/sys/class/leds/ev3:" + paParamList[2] + ":" + paParamList[1] + ":ev3dev"); //format in ev3dev is= ev3:[left | right]:[red | green]:ev3dev
 
-  sysFileName += ("red" == paParamList[1]) ? "0:" : "1:";
-  sysFileName += paParamList[1];
-  sysFileName += ":ev3dev/brightness";
+  sysFileName += "/brightness";
 
   mFile.open(sysFileName.c_str(), std::fstream::out);      //TODO change this when fully switching to C++11 for LMS EV3
 
@@ -108,23 +109,64 @@ bool CLMSEV3ProcessInterface::setupLEDOutput(const std::vector<std::string> &paP
 bool CLMSEV3ProcessInterface::setupSensor(const std::string &paParam){
   bool retVal = false;
 
-  std::string sysFileName(getSensorBasePath(*(paParam.rbegin())));
+  std::string sysFileName(getSensorBasePath(paParam));
+
   if(sysFileName.size()){
     sysFileName += "/value0";   //for now use the value file for getting the value. espeically for touch this seams to be the better option. Needs check for other sensors!!
     mFile.open(sysFileName.c_str(), std::fstream::in);      //TODO change this when fully switching to C++11 for LMS EV3
     if(mFile.is_open()){
       retVal = true;
     }
+
   }
   return retVal;
 }
 
-std::string CLMSEV3ProcessInterface::getSensorBasePath(char paSensorPortNum){
+std::string CLMSEV3ProcessInterface::getSensorBasePath(const std::string &paParam){
   std::string retVal;
-  if(('0' <= paSensorPortNum) && (paSensorPortNum < '5')){
-    //todo search for the correct sensor port using the port_name file
-    retVal = "/sys/class/lego-sensor/sensor";
-    retVal += paSensorPortNum;
+  std::string basePath = "/sys/class/lego-sensor/sensor";
+  int sensorNumber = -1;
+  std::stringstream number;
+
+  sensorNumber = findNumberFromPort(basePath, paParam);
+  if (sensorNumber != -1)
+  {
+	  retVal = basePath;
+	  number << sensorNumber;
+	  retVal += number.str();
   }
+
   return retVal;
+}
+
+int CLMSEV3ProcessInterface::findNumberFromPort(const std::string &paBasePath, const std::string &paEv3Port){
+	int retVal = -1;
+	std::fstream mAddressFile;
+	std::string fullPath;
+	char port[5];
+	int portLenght = paEv3Port.length();
+
+	for (int i = 0; i < 255; i++){ //TODO: check maximum value of i
+		std::stringstream number;
+		number << i;
+		fullPath = paBasePath + number.str() + "/address";
+
+		mAddressFile.open(fullPath.c_str(), std::fstream::in);      //TODO change this when fully switching to C++11 for LMS EV3ca
+
+		if(mAddressFile.is_open()){
+			mAddressFile.clear();
+			mAddressFile.seekg(0, std::ios::beg);
+			mAddressFile.read(port, portLenght);
+			mAddressFile.close();
+			port[portLenght] = '\0';
+
+			if (paEv3Port.compare(port) == 0)
+			{
+				retVal = i;
+				break;
+			}
+
+		}
+	}
+	return retVal;
 }
