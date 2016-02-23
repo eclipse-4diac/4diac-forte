f8faf0f0786d696071a59ce8a99dd5155f8a72fe
azoitl <alois.zoitl@gmx.at>
2016-02-23 14:34:42 +0100

    488262: Extract common thread functions into a platform independent thread base class
    added missing files for thread base class
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=488262

---
+++ src/arch/threadbase.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2016 fortiss GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Alois Zoitl - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+#include "threadbase.h"
+
+namespace forte {
+  namespace arch {
+
+    CThreadBase::CThreadBase() : mAlive(false){
+
+    }
+
+    CThreadBase::~CThreadBase(){
+    }
+
+
+  } /* namespace arch */
+} /* namespace forte */
---
+++ src/arch/threadbase.h
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2016 fortiss GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Alois Zoitl - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+#ifndef _THREADBASE_H_
+#define _THREADBASE_H_
+
+namespace forte {
+  namespace arch {
+
+    class CThreadBase{
+      public:
+        CThreadBase();
+
+        /*! \brief Indicates if the thread is allowed to execute.
+         *
+         *  This functions checks if the Thread is still executing user code in its run()-method.
+         *  \return true if there the run method is active.
+         */
+        bool isAlive(void) const{
+          return mAlive;
+        }
+
+
+      protected:
+        virtual ~CThreadBase();
+
+        /*! \brief set the alive flag for this flag
+         */
+        void setAlive(bool paVal) {
+          mAlive = paVal;
+        }
+
+        /*! \brief Abstract method for the code to execute in the thread.
+         *
+         *  This thread class has to provide means that the code a inheriting class will add to the run()-method will
+         *  be executed in a separated thread regarding the creator of the CThread class.
+         *
+         *  The inheriting class has to fulfill the following rules when using the run method:
+         *    - To end the thread execution simple leave the run()-method
+         *    - In order to allow the deletion and stopping of the thread add frequent checks to isAlive() and end the
+         *      execution if isAlive() returns false.
+         */
+        virtual void run() = 0;
+
+      private:
+
+        /*! \brief Flag that indicates if the Thread is alive.
+         *
+         *  This flag has two main purposes:
+         *    -# indicate for other classes if the thread is still executing
+         *    -# use in the run()-method to check if the thread is still allowed to execute (e.g. while(isAlive()) ).
+         *       This is important for stopping and destroying threads.
+         */
+        bool mAlive;
+
+        //we don't want that threads can be copied or assigned therefore the copy constructor and assignment operator are declared private
+        //but not implemented
+        CThreadBase(const CThreadBase&);
+        CThreadBase& operator = (const CThreadBase &);
+    };
+
+  } /* namespace arch */
+} /* namespace forte */
+
+#endif /* SRC_ARCH_THREADBASE_H_ */
