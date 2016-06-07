7e439a60df5c3f6ed5c797270bebd4a2e264be66
heichlergl5 <herwig.eichler@gmail.com>
2016-06-07 22:30:09 +0200

    [495477] Add conmeleon support to forte
    Added sysfs file access helper and uncopyable base class
    Changed code according to comments from Alois Zoitl.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=495477
    
    Signed-off-by: heichlergl5 <herwig.eichler@gmail.com>

---
+++ src/modules/conmeleon_c1/util/uncopyable.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2016 fortiss GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Herwig Eichler (conmeleon.org) - initial API and implementation and initial documentation
+ *******************************************************************************/
+
+#ifndef UTIL_UNCOPYABLE_H_
+#define UTIL_UNCOPYABLE_H_
+
+/*! \class CUncopyable
+ *  \brief CUncopyable is a base class for objects which are unique and should not be copied
+ *
+ * That's why the copy constructor and the assignment operator are private and not implemented, to prevent the compiler from creating it's own.
+ * The default constructor and the destructor are protected to allow derived classes implement their own constructors and destructors.
+ * Derived classes should be derived privately.
+ */
+class CUncopyable {
+protected:
+	CUncopyable() {}
+	~CUncopyable() {}
+private:
+	CUncopyable (const CUncopyable&);
+	CUncopyable& operator = (const CUncopyable&);
+};
+
+#endif /* UTIL_UNCOPYABLE_H_ */
