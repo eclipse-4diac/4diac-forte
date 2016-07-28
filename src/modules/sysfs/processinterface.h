68f9b3614cf2fc5687608c9b6035a3627f2d9f8e
azoitl <alois.zoitl@gmx.at>
2016-07-28 20:57:08 +0200

    [498753] Add support for Odriod Analog inputs
    Reworked sysfs module to allow it as base for other process interface
    implementations
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=498753
    Signed-off-by: azoitl <alois.zoitl@gmx.at>

--- src/modules/sysfs/processinterface.h
+++ src/modules/sysfs/processinterface.h
@@ -1,45 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2015 fortiss GmbH
+ * Copyright (c) 2016 fortiss GmbH
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Waldemar Eisenmenger - initial API and implementation and/or initial documentation
+ *    Alois Zoitl - initial API and implementation and/or initial documentation
  *******************************************************************************/
-
 #ifndef _PROCESSINTERFACE_H_
 #define _PROCESSINTERFACE_H_
 
-#include <../../stdfblib/io/processinterfacebase.h>
-#include <fstream>
-
-class CSysFsProcessInterface : public CProcessInterfaceBase{
-
-  public:
-    CSysFsProcessInterface(CResource *paSrcRes, const SFBInterfaceSpec *paInterfaceSpec, const CStringDictionary::TStringId paInstanceNameId, TForteByte *paFBConnData, TForteByte *paFBVarsData);
-    virtual ~CSysFsProcessInterface();
-
-  protected:
-    bool initialise(bool paIsInput);
-    bool deinitialise();
-    bool writePin();
-    bool readPin();
-
-  private:
-    std::fstream mFile;
-
-    static const char * const scmOK;
-    static const char * const scmPinInUse;
-    static const char * const scmNotInitialised;
-    static const char * const scmError;
-    static const char * const scmCouldNotRead;
-    static const char * const scmCouldNotWrite;
-
-};
+#include "sysfsprocint.h"
 
 //tell the IX and QX FB that this is the process interface to be used
 typedef CSysFsProcessInterface CProcessInterface;
 
-#endif /* PROCESSINTERFACE_H_ */
+#endif
---
+++ src/modules/sysfs/sysfsprocint.h
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2015 fortiss GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Waldemar Eisenmenger - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+#ifndef _SYSFSPROCINT_H_
+#define _SYSFSPROCINT_H_
+
+#include <../../stdfblib/io/processinterfacebase.h>
+#include <fstream>
+
+class CSysFsProcessInterface : public CProcessInterfaceBase{
+
+  public:
+    CSysFsProcessInterface(CResource *paSrcRes, const SFBInterfaceSpec *paInterfaceSpec, const CStringDictionary::TStringId paInstanceNameId, TForteByte *paFBConnData, TForteByte *paFBVarsData);
+    virtual ~CSysFsProcessInterface();
+
+  protected:
+    bool initialise(bool paIsInput);
+    bool deinitialise();
+    bool writePin();
+    bool readPin();
+
+  private:
+    std::fstream mFile;
+
+    static const char * const scmOK;
+    static const char * const scmPinInUse;
+    static const char * const scmNotInitialised;
+    static const char * const scmError;
+    static const char * const scmCouldNotRead;
+    static const char * const scmCouldNotWrite;
+
+};
+
+#endif /* PROCESSINTERFACE_H_ */
