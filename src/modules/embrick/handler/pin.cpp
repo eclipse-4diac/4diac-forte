211d6ffe65fc5949a1f235fdb36ee18f8c934b00
Johannes Meßmer <johannes.messmer@tum.de>
2016-11-15 13:19:53 +0100

    [507431] Add pin handler for emBRICK
    Add initial version of the pin handler. It is needed to active the slave
    select pin for SPI protocol. Therefore it only supports digital output
    GPIOs via the sysfs interface.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=507431
    Change-Id: I8e216dce60ad8ae7cf3e15b4d7ca509be5c0331d
    Signed-off-by: Johannes Meßmer <johannes.messmer@tum.de>

---
+++ src/modules/embrick/handler/pin.cpp
@@ -0,0 +1,123 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Johannes Messmer (admin@jomess.com)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Johannes Messmer - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+#include "pin.h"
+
+namespace EmBrick {
+
+const char * const PinHandler::scmFailedToOpenFile =
+		"Failed to open sysfs file.";
+const char * const PinHandler::scmFailedToWriteFile =
+		"Failed to write sysfs file.";
+const char * const PinHandler::scmNotInitialised =
+		"Failed to write to not initialised stream.";
+
+PinHandler::PinHandler(unsigned int pin) :
+		pin(pin) {
+	// Disable buffer to avoid latency
+	stream.rdbuf()->pubsetbuf(0, 0);
+
+	// Init pin
+	init();
+}
+
+PinHandler::~PinHandler() {
+	deInit();
+}
+
+void PinHandler::init() {
+	std::string fileName;
+	stream.clear();
+
+	// Prepare pin as std::string
+	std::ostringstream pinStream;
+	pinStream << pin;
+	std::string pinStr = pinStream.str();
+
+	// Enable pin
+	fileName = "/sys/class/gpio/export";
+	stream.open(fileName.c_str(), std::fstream::out);
+	if (!stream.is_open())
+		return fail(scmFailedToOpenFile);
+
+	stream << pin;
+	if (stream.fail())
+		return fail(scmFailedToWriteFile);
+	stream.close();
+
+	// Use pin as output
+	fileName = "/sys/class/gpio/gpio" + pinStr + "/direction";
+	if (!stream.is_open())
+		return fail(scmFailedToOpenFile);
+	stream.clear();
+
+	stream << "out";
+	if (stream.fail())
+		return fail(scmFailedToWriteFile);
+	stream.close();
+
+	// Prepare pin stream for usage
+	fileName = "/sys/class/gpio/gpio" + pinStr + "/value";
+	stream.open(fileName.c_str(), std::fstream::out);
+	if (!stream.is_open())
+		return fail(scmFailedToOpenFile);
+}
+
+void PinHandler::deInit() {
+	std::string fileName;
+
+	// Prepare pin as std::string
+	std::ostringstream pinStream;
+	pinStream << pin;
+	std::string pinStr = pinStream.str();
+
+	// Close pin stream
+	if (stream.is_open()) {
+		stream.close();
+		stream.clear();
+	}
+
+	// Disable pin
+	fileName = "/sys/class/gpio/unexport";
+	stream.open(fileName.c_str(), std::fstream::out);
+	if (!stream.is_open())
+		return fail(scmFailedToOpenFile);
+
+	stream << pin;
+	if (stream.fail())
+		return fail(scmFailedToWriteFile);
+	stream.close();
+}
+
+bool PinHandler::set(bool state) {
+	if (!stream.is_open()) {
+		fail(scmNotInitialised);
+		return false;
+	}
+
+	stream.clear();
+	stream.seekp(0, std::ios::beg);
+
+	unsigned int val = state ? 1 : 0;
+	stream << val;
+	if (stream.fail()) {
+		fail(scmFailedToWriteFile);
+		return false;
+	}
+
+	return true;
+}
+
+void PinHandler::fail(const char* reason) {
+	// TODO Implement error handler
+}
+
+} /* namespace EmBrick */
---
+++ src/modules/embrick/handler/pin.h
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Johannes Messmer (admin@jomess.com)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Johannes Messmer - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+#ifndef SRC_MODULES_EMBRICK_HANDLER_PIN_H_
+#define SRC_MODULES_EMBRICK_HANDLER_PIN_H_
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+namespace EmBrick {
+
+class PinHandler {
+public:
+	PinHandler(unsigned int pin);
+	virtual ~PinHandler();
+
+	bool set(bool state);
+	bool enable() {
+		return set(true);
+	}
+	bool disable() {
+		return set(false);
+	}
+
+protected:
+	void init();
+	void deInit();
+
+	unsigned int pin;
+	std::fstream stream;
+
+private:
+	void fail(const char* reason);
+
+	static const char * const scmFailedToOpenFile;
+	static const char * const scmFailedToWriteFile;
+	static const char * const scmNotInitialised;
+};
+
+} /* namespace EmBrick */
+
+#endif /* SRC_MODULES_EMBRICK_HANDLER_PIN_H_ */
