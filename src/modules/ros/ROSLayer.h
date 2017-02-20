5cad3b3096d1b74b82eff27a68115c2fd3196570
Ben Schneider <schneider@fortiss.org>
2017-02-20 15:20:00 +0100

    [512168] Initial ROS communication layer with topic support
    A communication layer was implemented in order to be able to send ROS
    topics via standard publish and subscribe SIFB that are configured for
    ROS. The currently supported topics are std_msgs/Empty,
    std_msgs/Float64, std_msgs/Int32, std_msgs/Bool and std_msgs/String.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=512168
    Change-Id: I2fb68977b026f3886bb85f514b63021d194861cf
    Signed-off-by: Ben Schneider <schneider@fortiss.org>

---
+++ src/modules/ros/ROSLayer.h
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2016 - 2017 fortiss GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ben Schneider
+ *      - initial implementation and documentation
+ *******************************************************************************/
+
+#ifndef CROSLAYER_H_
+#define CROSLAYER_H_
+
+#include <ros/ros.h>
+#include <ros/service_callback_helper.h>
+#include <topic_tools/shape_shifter.h>
+
+#include <comlayer.h>
+#include <extevhan.h>
+#include <thread.h>
+#include <singlet.h>
+
+#include <string>
+
+class CROSLayer : public forte::com_infra::CComLayer{
+
+  public:
+    CROSLayer(forte::com_infra::CComLayer* pa_poUpperLayer, forte::com_infra::CCommFB* pa_poComFB);
+    virtual ~CROSLayer();
+
+    virtual void closeConnection();
+
+    virtual forte::com_infra::EComResponse sendData(void *pa_pvData, unsigned int pa_unSize);
+    virtual forte::com_infra::EComResponse recvData(const void *pa_pvData, unsigned int pa_unSize);
+
+    virtual forte::com_infra::EComResponse processInterrupt();
+
+  private:
+    virtual forte::com_infra::EComResponse openConnection(char *pa_acLayerParameter);
+
+    forte::com_infra::EComServiceType m_eCommServiceType;
+    forte::com_infra::EComResponse m_eInterruptResp;
+
+    ros::NodeHandle m_Nh;
+    ros::Publisher m_Pub;
+    ros::Subscriber m_Sub;
+    std::string m_TopicName;
+    std::string m_TopicType;
+    int m_NumRDs;
+    int m_NumSDs;
+
+    void handleReceivedValue(const boost::shared_ptr<const topic_tools::ShapeShifter>& pa_message);
+
+    //TODO refactor ROSActionManager and CROSManager (in class CROSLayer)
+
+    class CROSManager : public CExternalEventHandler, private CThread{
+      DECLARE_SINGLETON(CROSManager)
+
+      public:
+        virtual void enableHandler();
+        virtual void disableHandler();
+        virtual void setPriority(int);
+        virtual int getPriority() const;
+
+        void startChain(CEventSourceFB *pa_poECStartF);
+
+      protected:
+        virtual void run();
+
+    };
+};
+
+#endif //_CROSLAYER_H_
