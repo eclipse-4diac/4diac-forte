ceabd94263f716258f1c133f15bbc7eb3369aa66
CapXilinx <melik-merkumians@acin.tuwien.ac.at>
2018-02-05 10:00:19 +0100

    Fixes incorrect headers for files commited in 47286d695473135b0fb02297296af98156e4669a
    Signed-off-by: CapXilinx <melik-merkumians@acin.tuwien.ac.at>

--- src/arch/posix/pctimeha.cpp
+++ src/arch/posix/pctimeha.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 - 2013 ACIN, fortiss GmbH
+ * Copyright (c) 2005 - 2018 ACIN, fortiss GmbH
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
