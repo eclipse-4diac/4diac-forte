c2de72ca0ca871c725808ef3558bccb32368cb48
Martin Melik Merkumians <melik-merkumians@acin.tuwien.ac.at>
2018-07-09 12:46:17 +0200

    [520730] Limits inital semaphore value in Windows to 1
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=520730
    
    Change-Id: I36cd688099af4f8bfa9912743df6053a89de2f61
    Signed-off-by: Martin Melik Merkumians <melik-merkumians@acin.tuwien.ac.at>

--- src/arch/win32/forte_sem.cpp
+++ src/arch/win32/forte_sem.cpp
@@ -20,7 +20,7 @@ namespace forte {
   namespace arch {
 
     CWin32Semaphore::CWin32Semaphore(unsigned int paInitialValue){
-      mSemaphore = CreateSemaphore(NULL, paInitialValue, 1, NULL);
+      mSemaphore = CreateSemaphore(NULL, (paInitialValue > 1 ? 1 : 0), 1, NULL);
       if(0 == mSemaphore){
         DEVLOG_ERROR("Could not initialize suspend semaphore: %d\n", GetLastError());
       }
