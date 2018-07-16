bca4aea382db6874e91240febfc298c0c24afca4
Jose Cabral <cabral@fortiss.org>
2018-07-16 15:56:05 +0200

    [HOTFIX] Fix include header for embrick
    Use the right path to the timespec include in embrick
    
    Signed-off-by: Jose Cabral <cabral@fortiss.org>

--- src/modules/embrick/handler/bus.cpp
+++ src/modules/embrick/handler/bus.cpp
@@ -20,7 +20,7 @@
 #include <slave/handles/analog.h>
 #include <slave/handles/analog10.h>
 #include "criticalregion.h"
-#include <timespec_utils.h>
+#include <utils/timespec_utils.h>
 
 const char * const EmbrickBusHandler::scmSlaveUpdateFailed = "Update of slave failed.";
 const char * const EmbrickBusHandler::scmNoSlavesFound = "No slave modules found.";
