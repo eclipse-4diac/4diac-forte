0255c755fa33834413112eb2f6ce1f2f1df120fe
Jose Cabral <cabral@fortiss.org>
2018-11-05 09:44:32 +0100

    Remove exceptions from tests compilation
    Disabling exceptions in the compilation will make boost ask for a self
    implementation of the throw_exception function, which is dummy
    implemented in this patch
    
    Change-Id: Iffd9e07ca9e19d98571bdd45454dd143cb11c6b4
    Signed-off-by: Jose Cabral <cabral@fortiss.org>

--- tests/forte_boost_tester.cpp
+++ tests/forte_boost_tester.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 ACIN
+ * Copyright (c) 2011, 2018 ACIN, fortiss GmbH
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,11 @@
  *
  * Contributors:
  *   Ingo Hegny - initial API and implementation and/or initial documentation
+ *   Jose Cabral - Add dummy function for throw_exception
  *******************************************************************************/
 #define BOOST_TEST_MODULE FORTE_TESTS
 #include <boost/test/included/unit_test.hpp>
 
+void boost::throw_exception(std::exception const&) {
+  //dummy
+}
