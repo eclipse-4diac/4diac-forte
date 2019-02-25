d54af3710be817b6850318ebd7260b242159203e
Jose Cabral <cabral@fortiss.org>
2019-02-25 15:26:07 +0100

    [541846] Clean sysfs and add handler to trigger changes in inputs
    The ugly log is gone and a handler for changing of inputs was added
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=541846
    Change-Id: Ia7a2db0c439fbc0eb37caa66bf83d194f21bf49f
    Signed-off-by: Jose Cabral <cabral@fortiss.org>

--- src/modules/sysfs/sysfsprocint.h
+++ src/modules/sysfs/sysfsprocint.h
@@ -13,19 +13,43 @@
 #define _SYSFSPROCINT_H_
 
 #include "../../stdfblib/io/processinterfacebase.h"
+#include <forte_thread.h>
+#include <extevhan.h>
 #include <fstream>
 
-class CSysFsProcessInterface : public CProcessInterfaceBase{
+class CSysFsProcessInterface : public CProcessInterfaceBase {
 
   public:
-    CSysFsProcessInterface(CResource *paSrcRes, const SFBInterfaceSpec *paInterfaceSpec, const CStringDictionary::TStringId paInstanceNameId, TForteByte *paFBConnData, TForteByte *paFBVarsData);
+    CSysFsProcessInterface(CResource *paSrcRes, const SFBInterfaceSpec *paInterfaceSpec, const CStringDictionary::TStringId paInstanceNameId,
+        TForteByte *paFBConnData, TForteByte *paFBVarsData);
     virtual ~CSysFsProcessInterface();
+
+    class CIOHandler : public CExternalEventHandler, public CThread {
+      DECLARE_HANDLER(CIOHandler)
+        ;
+      public:
+        virtual void run();
+        void updateReadData();
+        void registerIXFB(CSysFsProcessInterface *paFB);
+        void unregisterIXFB(CSysFsProcessInterface *paFB);
+        /* functions needed for the external event handler interface */
+        void enableHandler(void);
+        void disableHandler(void);
+        void setPriority(int paPriority);
+        int getPriority(void) const;
+
+      private:
+        typedef CSinglyLinkedList<CSysFsProcessInterface *> TReadFBContainer;
+        TReadFBContainer mReadFBList;
+        CSyncObject mReadFBListSync;
+    };
 
   protected:
     bool initialise(bool paIsInput);
     bool deinitialise();
     bool writePin();
     bool readPin();
+    bool checkInputData();
 
   private:
     std::fstream mFile;
@@ -33,15 +57,15 @@ class CSysFsProcessInterface : public CProcessInterfaceBase{
     bool exportGPIO();
     bool setDirection(bool paIsInput);
     bool valueGPIO(bool paIsInput);
-    
+
+    bool unexportIO();
+
     static const char * const scmOK;
     static const char * const scmPinInUse;
     static const char * const scmNotInitialised;
     static const char * const scmError;
     static const char * const scmCouldNotRead;
     static const char * const scmCouldNotWrite;
-
-
 };
 
 #endif /* PROCESSINTERFACE_H_ */
