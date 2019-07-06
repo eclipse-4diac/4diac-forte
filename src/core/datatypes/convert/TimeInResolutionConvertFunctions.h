75bce22e873fa26bd8a569dea52fe8fc545bbf75
Martin Melik-Merkumians <melik-merkumians@acin.tuwien.ac.at>
2019-07-06 08:08:29 +0200

    [547973] Adds TIME_IN_US_TO_LREAL
    Adds TIME_IN_US_TO_LREAL function and tests
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=547973
    
    Change-Id: Idf5ca5b01d39e2c5984ed77f8cac7a1ade16b157
    Signed-off-by: Martin Melik-Merkumians <melik-merkumians@acin.tuwien.ac.at>

--- src/core/datatypes/convert/TimeInResolutionConvertFunctions.h
+++ src/core/datatypes/convert/TimeInResolutionConvertFunctions.h
@@ -24,7 +24,8 @@ template<typename T>
 inline const T TIME_IN_S_TO(const CIEC_TIME &paValue) {
   FORTE_STATIC_ASSERT((forte::core::mpl::is_base_of<CIEC_ANY_NUM, T>::value), TNotOfAnyNum);
   if(forte::core::mpl::is_base_of<CIEC_ANY_REAL, T>::value) {
-    return T(static_cast<typename T::TValueType>(paValue.getInNanoSeconds()) / static_cast<typename T::TValueType>(forte::core::constants::cNanosecondsPerSecond));
+    return T(
+      static_cast<typename T::TValueType>(paValue.getInNanoSeconds()) / static_cast<typename T::TValueType>(forte::core::constants::cNanosecondsPerSecond));
   } else {
     return T(static_cast<typename T::TValueType>(paValue.getInSeconds()));
   }
@@ -35,8 +36,9 @@ inline const T TIME_IN_MS_TO(const CIEC_TIME &paValue) {
   FORTE_STATIC_ASSERT((forte::core::mpl::is_base_of<CIEC_ANY_NUM, T>::value), TNotOfAnyNum);
   if(forte::core::mpl::is_base_of<CIEC_ANY_REAL, T>::value) {
     return T(
-      static_cast<typename T::TValueType>(paValue.getInNanoSeconds()) *
-        (static_cast<typename T::TValueType>(forte::core::constants::cMillisecondsPerSecond) / static_cast<typename T::TValueType>(forte::core::constants::cNanosecondsPerSecond)));
+      static_cast<typename T::TValueType>(paValue.getInNanoSeconds())
+        * (static_cast<typename T::TValueType>(forte::core::constants::cMillisecondsPerSecond)
+          / static_cast<typename T::TValueType>(forte::core::constants::cNanosecondsPerSecond)));
   } else {
     return T(static_cast<typename T::TValueType>(paValue.getInMilliSeconds()));
   }
@@ -44,7 +46,15 @@ inline const T TIME_IN_MS_TO(const CIEC_TIME &paValue) {
 
 template<typename T>
 inline const T TIME_IN_US_TO(const CIEC_TIME &paValue) {
-  return T(static_cast<typename T::TValueType>(paValue.getInMicroSeconds()));
+  FORTE_STATIC_ASSERT((forte::core::mpl::is_base_of<CIEC_ANY_NUM, T>::value), TNotOfAnyNum);
+  if(forte::core::mpl::is_base_of<CIEC_ANY_REAL, T>::value) {
+    return T(
+      static_cast<typename T::TValueType>(paValue.getInNanoSeconds())
+        * (static_cast<typename T::TValueType>(forte::core::constants::cMicrosecondsPerSecond)
+          / static_cast<typename T::TValueType>(forte::core::constants::cNanosecondsPerSecond)));
+  } else {
+    return T(static_cast<typename T::TValueType>(paValue.getInMicroSeconds()));
+  }
 }
 
 template<typename T>
@@ -52,7 +62,10 @@ inline const T TIME_IN_NS_TO(const CIEC_TIME &paValue) {
   return T(static_cast<typename T::TValueType>(paValue.getInNanoSeconds()));
 }
 
-/* TO_LINT functions */
+/********************************************************************
+ *  TO_LINT functions
+ ********************************************************************/
+
 inline const CIEC_LINT TIME_IN_S_TO_LINT(const CIEC_TIME &paValue) {
   return TIME_IN_S_TO<CIEC_LINT>(paValue);
 }
@@ -69,7 +82,10 @@ inline const CIEC_LINT TIME_IN_NS_TO_LINT(const CIEC_TIME &paValue) {
   return TIME_IN_NS_TO<CIEC_LINT>(paValue);
 }
 
-/* TO_ULINT functions */
+/********************************************************************
+ *  TO_UINT functions
+ ********************************************************************/
+
 inline const CIEC_ULINT TIME_IN_S_TO_ULINT(const CIEC_TIME &paValue) {
   return TIME_IN_S_TO<CIEC_ULINT>(paValue);
 }
@@ -86,12 +102,20 @@ inline const CIEC_ULINT TIME_IN_NS_TO_ULINT(const CIEC_TIME &paValue) {
   return TIME_IN_NS_TO<CIEC_ULINT>(paValue);
 }
 
+/********************************************************************
+ *  TO_LREAL functions
+ ********************************************************************/
+
 inline const CIEC_LREAL TIME_IN_S_TO_LREAL(const CIEC_TIME &paValue) {
   return TIME_IN_S_TO<CIEC_LREAL>(paValue);
 }
 
 inline const CIEC_LREAL TIME_IN_MS_TO_LREAL(const CIEC_TIME &paValue) {
   return TIME_IN_MS_TO<CIEC_LREAL>(paValue);
+}
+
+inline const CIEC_LREAL TIME_IN_US_TO_LREAL(const CIEC_TIME &paValue) {
+  return TIME_IN_US_TO<CIEC_LREAL>(paValue);
 }
 
 #endif /* FORTE_USE_64BIT_DATATYPES */
