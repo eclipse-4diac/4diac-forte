90f374ecfcab36222ae82006513db92aa6724a76
Jörg Walter <joerg.walter@offis.de>
2020-11-26 03:26:10 +0100

    [569184] Introduce WinXP compatibility setting
    At least one major PLC vendor sold Windows XP based boxes, which means
    that this is a relevant target for 4diac. Right now, the
    platform-specific part of FORTE uses some >= Vista APIs.
    
    This patch introduces a workaround which makes CPCSyncObject use a
    critical section as backend. Due to possible performance degradation of
    critical sections vs. SRW locks, this change is guarded by a CMake
    setting.
    
    Furthermore, the patch replaces calls to InetPton with equivalent calls
    to WSAStringToAddressA.
    
    This patch also silences a few warnings on MingW.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=569184
    Signed-off-by: Jörg Walter <joerg.walter@offis.de>
    Change-Id: I24eba08ffed89be13a8b0d66209407fa22e3bf7b

--- src/arch/win32/forte_sync.h
+++ src/arch/win32/forte_sync.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 - 2013, 2017 ACIN, Profactor GmbH, fortiss GmbH
+ * Copyright (c) 2010, 2020 ACIN, Profactor GmbH, fortiss GmbH, OFFIS e.V.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *  Alois Zoitl, Gerhard Ebenhofer, Martin Melik-Merkumians - initial API and implementation and/or initial documentation
+ *  Jörg Walter - Windows XP compatibility
  *******************************************************************************/
 #ifndef _FORTE_SYNC_H_
 #define _FORTE_SYNC_H_
@@ -41,7 +42,14 @@ class CPCSyncObject{
     void unlock(void);
 
   private:
+    CPCSyncObject(const CPCSyncObject &); /* = delete; */
+    CPCSyncObject &operator=(const CPCSyncObject &); /* = delete; */
+
+#ifdef WINDOWS_XP_COMPAT
+    CRITICAL_SECTION mLock;
+#else
     SRWLOCK mLock;
+#endif
 };
 
 #endif /*FORTE_SYNC_H_*/
