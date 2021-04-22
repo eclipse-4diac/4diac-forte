e3761778c6b1fadf1e8d179e979e56f157a1fa49
Thomas Wagner <thomas.wagner@sysgo.com>
2021-04-22 13:33:13 +0200

    Bug 573082 - Adding PikeOS support independent of version/processor
    PikeOS was implemented with fixed values regarding target architecture.
    This patch uses configuration values of a PikeOS required PikeOS
    application project.
    This allows compiling Forte for each PikeOS version and target
    architecture.
    
    Change-Id: Ic5a7242ffcbf99f07d895f673016f9f990335969
    Signed-off-by: thomas.wagner@sysgo.com

---
+++ src/arch/pikeos_posix/forte_sem.cpp
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2021 SYSGO GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Alois Zoitl - initial API and implementation and/or initial documentation
+ *  Peter Gsellmann, Martin Melik-Merkumians - adds timed wait and try and no wait
+ *    and documentation
+ *  Thomas Wagner - copy from "arch/posix": CLOCK_MONOTONIC was replaced by
+ *                  CLOCK_REALTIME, which is required by PikeOS Posix.
+ *******************************************************************************/
+
+#include <errno.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include "forte_sem.h"
+#include "../devlog.h"
+#include "../utils/timespec_utils.h"
+
+namespace forte {
+  namespace arch {
+
+    CPThreadSemaphore::CPThreadSemaphore(unsigned int paInitialValue){
+      if(-1 == sem_init(&mSemaphore, 0, paInitialValue)){
+        DEVLOG_ERROR("Could not initialize suspend sempaphore: %s\n", strerror(errno));
+      }
+    }
+
+    CPThreadSemaphore::~CPThreadSemaphore(){
+      sem_destroy(&mSemaphore);
+    }
+
+    void CPThreadSemaphore::inc(){
+      sem_post(&mSemaphore);
+    }
+
+    void CPThreadSemaphore::waitIndefinitely(){
+      while((-1 == sem_wait(&mSemaphore)) && (errno == EINTR)); //handle interrupts from signals
+    }
+
+    bool CPThreadSemaphore::timedWait(const TForteUInt64 paRelativeTimeout){
+      timespec timeoutSpec = { static_cast<time_t>(paRelativeTimeout / scmSecondInNanoSeconds), static_cast<time_t>(paRelativeTimeout % scmSecondInNanoSeconds) };
+      timespec currentTime = { 0, 0 };
+      clock_gettime(CLOCK_REALTIME, &currentTime);
+
+      timespec expectedAbsoluteTimeoutTime = {0, 0};
+      timespecAdd(&currentTime, &timeoutSpec, &expectedAbsoluteTimeoutTime);
+
+      do{
+        if(0 == sem_trywait(&mSemaphore)){
+          return true;
+        }
+        clock_gettime(CLOCK_REALTIME, &currentTime);
+
+      } while(timespecLessThan(&currentTime, &expectedAbsoluteTimeoutTime));
+      return false;
+    }
+
+    bool CPThreadSemaphore::tryNoWait(){
+      return (0 == sem_trywait(&mSemaphore));
+    }
+  } /* namespace arch */
+} /* namespace forte */
---
+++ src/arch/pikeos_posix/forte_thread.cpp
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2021 SYSGO GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Alois Zoitl, Rene Smodic, Martin Melik Merkiumians - initial API and implementation and/or initial documentation
+ *  Alois Zoitl - extracted common functions to new base class CThreadBase
+ *  Jens Reimann - fix waiting for thread initialization
+ *  Thomas Wagner - copy from "arch/posix": removing #include <sys/wait.h>
+ *******************************************************************************/
+#include <fortealloc.h>
+#include "forte_thread.h"
+#include "../devlog.h"
+#include <errno.h>
+#include <cstring>
+#include <signal.h>
+#include <unistd.h>
+#include <criticalregion.h>
+
+forte::arch::CThreadBase<pthread_t>::TThreadHandleType CPosixThread::createThread(long paStackSize){
+  TThreadHandleType retVal = 0;
+
+  if(paStackSize){
+    pthread_attr_t stAttr;
+
+    if(pthread_attr_init(&stAttr)){
+      DEVLOG_ERROR("Error could not get the default thread attributes! %s\n", strerror(errno));
+      return 0;
+    }
+#ifdef __CYGWIN__
+    if (pthread_attr_setstacksize (&stAttr, paStackSize)){
+      DEVLOG_ERROR("Error could not set the stacksize for the thread! %s\n", strerror(errno));
+      return 0;
+    }
+#else //__CYGWIN__
+    if(pthread_attr_setstack(&stAttr, mStack, paStackSize)){
+      DEVLOG_ERROR("Error could not set the stacksize for the thread! %s\n", strerror(errno));
+      return 0;
+    }
+#endif //__CYGWIN__
+    if(pthread_create(&retVal, &stAttr, threadFunction, this)){
+      DEVLOG_ERROR("Error could not create the thread! %s\n", strerror(errno));
+      return 0;
+    }
+    if(pthread_attr_destroy(&stAttr)){
+      DEVLOG_ERROR("Error could not free the thread attributes! %s\n", strerror(errno));
+      return 0;
+    }
+  }
+  else{
+    if(pthread_create(&retVal, NULL, threadFunction, this)){
+      DEVLOG_ERROR("Error could not create the thread! %s\n", strerror(errno));
+      return 0;
+    }
+  }
+  // Detach because we don't care about the thread anymore/don't need to join. To cleanup either call pthread_detach or pthread_join
+  pthread_detach(retVal);
+
+  return retVal;
+}
+
+void * CPosixThread::threadFunction(void *paArguments){
+  // Get pointer to CThread object out of void pointer
+  CThreadBase<pthread_t>::runThread(static_cast<CPosixThread *>(paArguments));
+  return 0;
+}
+
+CPosixThread::CPosixThread(long paStackSize) : CThreadBase<pthread_t>(paStackSize){
+  if(0 != paStackSize){
+    mStack = new char[paStackSize];
+  }
+}
+
+CPosixThread::~CPosixThread(){
+}
+
+void CPosixThread::setDeadline(const CIEC_TIME &paVal){
+  mDeadline = paVal;
+  //under the posix pthread implemention currently it makes no sense to set any priority.
+  //It will not be considered.
+}
+
+void CPosixThread::sleepThread(unsigned int paMilliSeconds){
+  struct timespec stReq = { static_cast<time_t>(paMilliSeconds / 1000), static_cast<long>(1000000 * (paMilliSeconds % 1000)) };
+  nanosleep(&stReq, NULL);
+}
