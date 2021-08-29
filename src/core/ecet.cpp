0e57fe88d7ac83bc2ded71108dd5e684bae990a6
xzhaoybk <zhaoxin@hitrobotgroup.com>
2021-08-29 22:46:14 +0800

    [574318] External event queue becomes event locker after it is full
    To solve the problem that after the queue is full,
    the event chain logic cannot be executed any more
    
    Bug:https://bugs.eclipse.org/bugs/show_bug.cgi?id=574318
    Signed-off-by: xzhaoybk <zhaoxin@hitrobotgroup.com>

--- src/core/ecet.cpp
+++ src/core/ecet.cpp
@@ -1,17 +1,19 @@
-/*******************************************************************************
- * Copyright (c) 2005 - 2015 ACIN, Profactor GmbH, fortiss GmbH
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *    Alois Zoitl, Thomas Strasser, Rene Smodic, Gerhard Ebenhofer, Ingo Hegny
- *    Micheal Hofmann
- *      - initial implementation and rework communication infrastructure
- *******************************************************************************/
+ /*******************************************************************************
+  * Copyright (c) 2005, 2021 ACIN, Profactor GmbH, fortiss GmbH, Hit robot group
+  *
+  * This program and the accompanying materials are made available under the
+  * terms of the Eclipse Public License 2.0 which is available at
+  * http://www.eclipse.org/legal/epl-2.0.
+  *
+  * SPDX-License-Identifier: EPL-2.0
+  *
+  * Contributors:
+  *    Alois Zoitl, Thomas Strasser, Rene Smodic, Gerhard Ebenhofer, Ingo Hegny
+  *    Micheal Hofmann
+  *      - initial implementation and rework communication infrastructure
+  *    zhaoxin
+  *      -  fix that external event queue becomes event locker after it is full
+  *******************************************************************************/
 #include <forte_config.h>
 #include <fortenew.h>
 #include "ecet.h"
@@ -20,8 +22,7 @@
 #include "../arch/devlog.h"
 
 CEventChainExecutionThread::CEventChainExecutionThread() :
-    CThread(), mSuspendSemaphore(0), mProcessingEvents(false)
-{
+    CThread(), mSuspendSemaphore(0), mProcessingEvents(false){
   clear();
 }
 
@@ -87,6 +88,11 @@ void CEventChainExecutionThread::transferExternalEvents(){
         mExternalEventListStart--;
       }
     }
+  }
+  //the queue is full and pop out last one
+  if(0 != *mExternalEventListStart){
+      addEventEntry(*mExternalEventListStart);
+      *mExternalEventListStart = 0;
   }
 }
 
