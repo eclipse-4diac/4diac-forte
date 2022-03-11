1ee20082176111435aa7fb7261e3ef0e106dcb9f
Martin Melik-Merkumians <melik-merkumians@acin.tuwien.ac.at>
2022-03-11 12:20:33 +0100

    Bug 579171 ChangesCIEC_ULINT primitive argument ctor to explicit
    Signed-off-by: Martin Melik-Merkumians <melik-merkumians@acin.tuwien.ac.at>
    Change-Id: I7d7825150b805bfeeed96bab17a62b71f8dffc19

--- src/core/datatypes/forte_ulint.h
+++ src/core/datatypes/forte_ulint.h
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2009 - 2013 ACIN, fortiss GmbH
+ *               2022 Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -10,11 +11,15 @@
  * Contributors:
  *    Monika Wenger, Alois Zoitl
  *      - initial implementation and rework communication infrastructure
+ *    Martin Melik Merkumians - make TForteUInt64 constructor explicit
  *******************************************************************************/
 #ifndef _FORTE_ULINT_H_
 #define _FORTE_ULINT_H_
 
 #include "forte_any_int.h"
+#include "forte_udint.h"
+#include "forte_uint.h"
+#include "forte_usint.h"
 #include <limits>
 
 #ifdef FORTE_USE_64BIT_DATATYPES
@@ -38,10 +43,19 @@ class CIEC_ULINT : public CIEC_ANY_INT{
       setValueSimple(paValue);
     }
 
-    // We don't want this constructor to be explicit as it simplifies code generation for ST algorithms
-    // Maybe when we have better code generators we want to make this constructor explicit again and generate it
-    // cppcheck-suppress noExplicitConstructor
-   CIEC_ULINT(TForteUInt64 paValue){
+    CIEC_ULINT(const CIEC_UDINT& paValue) : CIEC_ANY_INT() {
+      setValueSimple(paValue);
+    }
+
+    CIEC_ULINT(const CIEC_UINT& paValue) : CIEC_ANY_INT() {
+      setValueSimple(paValue);
+    }
+
+    CIEC_ULINT(const CIEC_USINT& paValue) : CIEC_ANY_INT() {
+      setValueSimple(paValue);
+    }
+
+   explicit CIEC_ULINT(TForteUInt64 paValue){
       setTUINT64(paValue);
     }
 
@@ -56,12 +70,12 @@ class CIEC_ULINT : public CIEC_ANY_INT{
      *   \return Can be the following response:
      *     - Pointer to given object.
      */
-    CIEC_ULINT& operator =(TForteUInt64 paValue){
+    CIEC_ULINT& operator =(TForteUInt64 paValue) {
       setTUINT64(paValue);
       return *this;
     }
 
-    CIEC_ULINT& operator =(const CIEC_ULINT &paValue){
+    CIEC_ULINT& operator =(const CIEC_ULINT &paValue) {
       // Simple value assignment - no self assignment check needed
       setValueSimple(paValue);
       return *this;
@@ -71,11 +85,11 @@ class CIEC_ULINT : public CIEC_ANY_INT{
      *
      *   Conversion operator for converting CIEC_ULINT to elementary unsigned 64 bit integer
      */
-    operator TForteUInt64() const{
+    operator TForteUInt64() const {
       return getTUINT64();
     }
 
-    virtual EDataTypeID getDataTypeID() const{
+    virtual EDataTypeID getDataTypeID() const {
       return CIEC_ANY::e_ULINT;
     }
 };
