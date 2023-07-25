599ddaa2a435981f43f4e612863d14de0332d020
Martin Jobst <martin.jobst@posteo.de>
2023-07-25 14:27:34 +0200

    Bug 581784 - Add PUBLISH_1 and SUBSCRIBE_1 for use as internal FBs
    Change-Id: I6513af14289a58bc13a438c77922b28a6c53e1b5
    Signed-off-by: Martin Jobst <martin.jobst@posteo.de>

---
+++ src/stdfblib/net/PUBLISH_1.h
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Martin Erich Jobst
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Martin Jobst - initial implementation
+ *******************************************************************************/
+
+#pragma once
+
+#include "GEN_PUBLISH.h"
+
+class FORTE_PUBLISH_1 : public GEN_PUBLISH {
+  DECLARE_GENERIC_FIRMWARE_FB(FORTE_PUBLISH_1)
+
+  protected:
+    static const TEventID scm_nEventREQID = scm_nSendNotificationEventID;
+
+  public:
+    FORTE_PUBLISH_1(const CStringDictionary::TStringId pa_nInstanceNameId, CResource *pa_poSrcRes);
+    ~FORTE_PUBLISH_1() override = default;
+
+    void evt_INIT(const CIEC_BOOL &pa_QI, const CIEC_WSTRING &pa_ID, const CIEC_ANY &pa_SD_1, CIEC_BOOL &pa_QO, CIEC_WSTRING &pa_STATUS) {
+      QI() = pa_QI;
+      ID() = pa_ID;
+      getSDs()[0]->setValue(pa_SD_1.unwrap());
+      receiveInputEvent(scm_nEventINITID, nullptr);
+      pa_QO = QO();
+      pa_STATUS = STATUS();
+    }
+    void evt_REQ(const CIEC_BOOL &pa_QI, const CIEC_WSTRING &pa_ID, const CIEC_ANY &pa_SD_1, CIEC_BOOL &pa_QO, CIEC_WSTRING &pa_STATUS) {
+      QI() = pa_QI;
+      ID() = pa_ID;
+      getSDs()[0]->setValue(pa_SD_1.unwrap());
+      receiveInputEvent(scm_nEventREQID, nullptr);
+      pa_QO = QO();
+      pa_STATUS = STATUS();
+    }
+    void operator()(const CIEC_BOOL &pa_QI, const CIEC_WSTRING &pa_ID, const CIEC_ANY &pa_SD_1, CIEC_BOOL &pa_QO, CIEC_WSTRING &pa_STATUS) {
+      evt_INIT(pa_QI, pa_ID, pa_SD_1, pa_QO, pa_STATUS);
+    }
+};
---
+++ src/stdfblib/net/SUBSCRIBE_1.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Martin Erich Jobst
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Martin Jobst - initial implementation
+ *******************************************************************************/
+
+#include "SUBSCRIBE_1.h"
+#ifdef FORTE_ENABLE_GENERATED_SOURCE_CPP
+#include "SUBSCRIBE_1_gen.cpp"
+#endif
+
+DEFINE_GENERIC_FIRMWARE_FB(FORTE_SUBSCRIBE_1, g_nStringIdSUBSCRIBE_1)
+
+FORTE_SUBSCRIBE_1::FORTE_SUBSCRIBE_1(const CStringDictionary::TStringId pa_nInstanceNameId, CResource *pa_poSrcRes) :
+        GEN_SUBSCRIBE(pa_nInstanceNameId, pa_poSrcRes) {
+  configureFB("SUBSCRIBE_1");
+}
+
