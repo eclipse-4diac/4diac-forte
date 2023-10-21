6fd828de31d8f346633a39d961c796593eb7b6eb
Jörg Walter <joerg.walter@offis.de>
2023-10-21 21:11:52 +0200

    Bug 582573 - FreeRTOS-plus-TCP support
    Signed-off-by: Jörg Walter <joerg.walter@offis.de>
    Change-Id: I41d3898fac6dc77d47b39f69c7fb1ecf8829e634

--- src/arch/freeRTOS/sockhand.h
+++ src/arch/freeRTOS/sockhand.h
@@ -1,5 +1,5 @@
 /************************************************************************************
- * Copyright (c) 2016 fortiss GmbH
+ * Copyright (c) 2016, 2023 fortiss GmbH, OFFIS e.V.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -8,38 +8,21 @@
  *
  * Contributors:
  * Milan Vathoopan, Guru Chandrasekhara - initial API and implementation and/or initial documentation
+ * Jörg Walter - make lwIP exchangeable with FreeRTOS-plus-TCP
  ************************************************************************************/
 
 #ifndef SRC_ARCH_FREERTOS_SOCKHAND_H_
 #define SRC_ARCH_FREERTOS_SOCKHAND_H_
 
-#include "lwip/opt.h"
-#include "lwip/sockets.h"
-#include "lwip/api.h"
-#include "lwip/sys.h"
-#include "lwip/igmp.h"
-#include "lwip/inet.h"
-#include "lwip/tcp.h"
-#include "lwip/raw.h"
-#include "lwip/udp.h"
-#include "lwip/tcpip.h"
-
+#include <errno.h>
 #include <string.h>
-
 #include "devlog.h"
 
-#undef connect //gets confused with connect function of conn.h and childs
-#undef bind    //conflicts with a C++17 symbol used in FORTE
-
-#if !defined(CONFIG_IDF_CMAKE)
-inline int connect(int s, const struct sockaddr *name, socklen_t namelen) {
-  return lwip_connect(s, name, namelen);
-}
+#ifdef FORTE_FREERTOS_PLUS_TCP
+#include "sockhand_plustcp.h"
+#else
+#include "sockhand_lwip.h"
 #endif
-
-static inline int bind(int sockfd, struct sockaddr *addr, socklen_t addrlen) {
-  return lwip_bind(sockfd, addr, addrlen);
-}
 
 //these include needs to be last
 #include "../gensockhand.h"
