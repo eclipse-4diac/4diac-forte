fb7dd7c9afa2ba5e1be0e392bb4f25a99ffe30c8
Ketut Kumajaya <ketut.kumajaya@gmail.com>
2024-03-29 20:51:25 +0700

    win32: Use the standard C++ time functions
    This fix pre Windows 8/Server 2012 compatibility issue since the
    GetSystemTimePreciseAsFileTime function is not available in Windows 7

--- src/arch/win32/forte_architecture_time.cpp
+++ src/arch/win32/forte_architecture_time.cpp
@@ -2,6 +2,7 @@
  * Copyright (c) 2019 TU Wien/ACIN
  *               2022 Primetals Technologies Austria GmbH
  *               2023 Martin Erich Jobst
+ *               2024 Samator Indo Gas
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -12,37 +13,24 @@
  *  Martin Melik Merkumians - Adds getNanoSecondsMonotonic
  *                          - Optimize order of operations for time result
  *  Martin Jobst - add high-resolution realtime clock
+ *  Ketut Kumajaya - Use the standard C++ time functions, fix pre Windows 8/
+ *                   Server 2012 compatibility issue
  *******************************************************************************/
 
-#include <windows.h>
+#include <chrono>
+
+using namespace std::chrono;
 
 #include "forte_architecture_time.h"
 #include "forte_constants.h"
 
 #ifndef FORTE_FAKE_TIME
 uint_fast64_t getNanoSecondsMonotonic() {
-  LARGE_INTEGER performance_counter;
-  LARGE_INTEGER performance_frequency;
-
-  QueryPerformanceCounter(&performance_counter);
-  QueryPerformanceFrequency(&performance_frequency);
-
-  // The clock frequency for Win10 on the Performance Timer is 10Mhz or less
-  // in previous versions accoording to
-  // https://stackoverflow.com/questions/63205226/better-than-100ns-resolution-timers-in-windows
-  // so it is safe to assume that forte::core::constants::cNanosecondsPerSecond >> performance_frequency.QuadPart
-  const uint_fast64_t timeFactor = static_cast<uint_fast64_t>(forte::core::constants::cNanosecondsPerSecond) / static_cast<uint_fast64_t>(performance_frequency.QuadPart);
-  return static_cast<uint_fast64_t>(performance_counter.QuadPart) * timeFactor;
+  return static_cast<uint_fast64_t>(duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count());
 }
 
 uint_fast64_t getNanoSecondsRealtime() {
-  FILETIME filetime;
-
-  GetSystemTimePreciseAsFileTime(&filetime);
-
-  // The FILETIME struct stores the number of 100-nanosecond intervals since January 1, 1601 (UTC).
-  uint_fast64_t time = static_cast<uint_fast64_t>(filetime.dwHighDateTime) << 32 | static_cast<uint_fast64_t>(filetime.dwLowDateTime);
-  return (time - 116444736000000000LL) * 100LL; // convert to Unix Epoch and nanoseconds
+  return static_cast<uint_fast64_t>(duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count());
 }
 #endif
 
