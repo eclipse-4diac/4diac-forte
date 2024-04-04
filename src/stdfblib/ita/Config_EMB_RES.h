4c877483c5509fb62919ecfd033df29db099aca9
Markus Meingast <markus.meingast@jku.at>
2024-04-04 15:30:10 +0200

    Add Config_EMB_RES class

---
+++ src/stdfblib/ita/Config_EMB_RES.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Primetals Technologies Austria GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Markus Meingast
+ *    - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+#pragma once
+
+#include "EMB_RES.h"
+
+class Config_EMB_RES : public EMB_RES {
+  DECLARE_FIRMWARE_FB(Config_EMB_RES);
+
+  public:
+    Config_EMB_RES(CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paDevice);
+    ~Config_EMB_RES() override;
+
+  private:
+    static const SFBInterfaceSpec scmFBInterfaceSpec;
+
+    static const CStringDictionary::TStringId scmVarInputNameIds[];
+    static const CStringDictionary::TStringId scmDIDataTypeIds[];
+};
