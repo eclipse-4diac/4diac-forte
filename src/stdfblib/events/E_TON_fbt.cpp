f0e23626da2d2676cf946014626f595e8c32027b
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2024-04-23 12:40:13 +0200

    Update copyright information in E_TON_fbt files.
    Updated the copyright notice to reflect 2024 for HR Agrartechnik GmbH in the E_TON_fbt.cpp and E_TON_fbt.h files.

--- src/stdfblib/events/E_TON_fbt.cpp
+++ src/stdfblib/events/E_TON_fbt.cpp
@@ -1,4 +1,4 @@
-/*************************************************************************  
+/*************************************************************************
  *** Copyright (c) 2024 HR Agrartechnik GmbH  
  *** This program and the accompanying materials are made available under the  
  *** terms of the Eclipse Public License 2.0 which is available at  
