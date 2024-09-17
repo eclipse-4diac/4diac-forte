141c02c11fbe349ea3a664302de281ed8ef2e45b
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2024-09-17 23:01:56 +0200

    Update copyright year to 2024 in multiple files. Update copyright year to 2024 in multiple files.
    Updated the copyright year from 2022-2023 to 2022, 2024 in various files. Contributing authors and license details were also adjusted accordingly.

--- src/modules/storage_esp32/nvs/NVS_fbt.cpp
+++ src/modules/storage_esp32/nvs/NVS_fbt.cpp
@@ -1,5 +1,10 @@
 /*************************************************************************
- *** FORTE Library Element
+ *** Copyright (c) 2022, 2024 HR Agrartechnik GmbH
+ *** This program and the accompanying materials are made available under the
+ *** terms of the Eclipse Public License 2.0 which is available at
+ *** http://www.eclipse.org/legal/epl-2.0.
+ ***
+ *** SPDX-License-Identifier: EPL-2.0
  ***
  *** This file was generated using the 4DIAC FORTE Export Filter V1.0.x NG!
  ***
