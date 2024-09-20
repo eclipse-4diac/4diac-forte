90c2a6a9d94e44767ce7c0ef0676d59ccdf0b316
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2024-09-20 08:51:50 +0200

    Update copyright year to 2024 in source code files.
    Changed the copyright year from 2023 to 2024 in the license information of the source code files.

--- src/modules/signalprocessing/SCALE_ESC_fct.h
+++ src/modules/signalprocessing/SCALE_ESC_fct.h
@@ -1,5 +1,5 @@
 /*************************************************************************  
- *** Copyright (c) 2023 HR Agrartechnik GmbH  
+ *** Copyright (c) 2024 HR Agrartechnik GmbH
  *** This program and the accompanying materials are made available under the  
  *** terms of the Eclipse Public License 2.0 which is available at  
  *** http://www.eclipse.org/legal/epl-2.0.  
