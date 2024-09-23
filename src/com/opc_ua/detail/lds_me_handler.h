383e444906152e53ddd9a431d401e5625eed4a04
Jose Cabral <cabralcochi@gmail.com>
2024-09-23 08:06:41 +0200

    add documentation

--- src/com/opc_ua/detail/lds_me_handler.h
+++ src/com/opc_ua/detail/lds_me_handler.h
@@ -17,14 +17,32 @@
 
 #include <open62541.h>
 
-#include <vector>
 #include <string>
+#include <vector>
 
 namespace forte::com::opc_ua::detail {
 
+/**
+ * @brief Handles all things related to LDS-ME
+ * 
+ * Configures the server to accept LDS-ME messages and handles them accordingly.
+ */
 class LdsMeHandler {
   public:
+
+    /**
+     * @brief Constructor
+     * 
+     * @param paUaServer OpcUa server to handle
+     */
     LdsMeHandler(UA_Server& paUaServer);
+
+    /**
+     * @brief Sets the proper values to the server configuration so it accepts LDS-ME messages
+     * 
+     * @param paUaServerConfig configuration to be set
+     * @param paServerName server name to be used when registering to the discovery server 
+     */
     static void configureServer(UA_ServerConfig& paUaServerConfig, const std::string& paServerName);
   
   private:
@@ -35,6 +53,11 @@ class LdsMeHandler {
 
     UA_Server& mUaServer;
 
+    /**
+     * @brief For each discovery server, this class handles the opcua client connectig to it
+     * and the memory for the string holding the discovery url.
+     * 
+     */
     class ClientWithLds {
       public:
         ClientWithLds(const UA_String& paDiscoveryUrl);
@@ -50,9 +73,7 @@ class LdsMeHandler {
         UA_Client *mClient;
     };
 
-    /**
-     * List of LDS servers where this instance is already registered.
-     */
+    /// List of discovery servers where this instance is already registered
     std::vector<ClientWithLds> mRegisteredWithLds;
 };
 }
