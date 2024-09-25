659f495faf8a7ef126fa21fb4e13e7f1362f3ab5
Martin Jobst <martin.jobst@posteo.de>
2024-09-25 17:54:13 +0200

    Add generic event accessors
    Add generic event accessors to enable usage of network FBs as internal FBs.

--- src/stdfblib/net/GEN_SERVER_fbt.h
+++ src/stdfblib/net/GEN_SERVER_fbt.h
@@ -1,5 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2006 - 2011 ACIN, Profactor GmbH
+ * Copyright (c) 2006, 2024 ACIN, Profactor GmbH
+ *                          Martin Erich Jobst
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -9,6 +11,8 @@
  * Contributors:
  *   Rene Smodic, Alois Zoitl, Gerhard Ebenhofer, Martin Melik Merkumians
  *    - initial API and implementation and/or initial documentation
+ *   Martin Erich Jobst
+ *    - add generic event accessors
  *******************************************************************************/
 #ifndef _GEN_SERVER_H_
 #define _GEN_SERVER_H_
@@ -19,12 +23,31 @@
  *
  */
 
-class GEN_SERVER: public forte::com_infra::CCommFB {
-    DECLARE_GENERIC_FIRMWARE_FB(GEN_SERVER)
+class GEN_SERVER : public forte::com_infra::CCommFB {
+  DECLARE_GENERIC_FIRMWARE_FB(GEN_SERVER)
   public:
     GEN_SERVER(const CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer);
+
     ~GEN_SERVER() override = default;
 
+    template<typename ...Args>
+    void evt_INIT(Args &&...paArgs) {
+      writeArguments(std::forward<Args>(paArgs)...); // write all arguments to get type information
+      receiveInputEvent(scmEventINITID, nullptr);
+      readOutputArguments(std::forward<Args>(paArgs)...);
+    }
+
+    template<typename ...Args>
+    void evt_RSP(Args &&...paArgs) {
+      writeInputArguments(std::forward<Args>(paArgs)...);
+      receiveInputEvent(scmSendNotificationEventID, nullptr);
+      readOutputArguments(std::forward<Args>(paArgs)...);
+    }
+
+    template<typename ...Args>
+    void operator()(Args &&...paArgs) {
+      evt_INIT(std::forward<Args>(paArgs)...);
+    }
 };
 
 #endif //_GEN_SERVER_H_
