43e2cc0143efdc3ce5d364ea3c0a569faac9c139
Martin Jobst <martin.jobst@posteo.de>
2024-09-25 17:54:13 +0200

    Add generic event accessors
    Add generic event accessors to enable usage of network FBs as internal FBs.

--- src/stdfblib/net/GEN_PUBLISH_fbt.h
+++ src/stdfblib/net/GEN_PUBLISH_fbt.h
@@ -1,5 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2006 - 2011 ACIN, Profactor GmbH
+ * Copyright (c) 2006, 2024 ACIN, Profactor GmbH
+ *                          Martin Erich Jobst
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -9,6 +11,8 @@
  * Contributors:
  *   Alois Zoitl, Rene Smodic, Gerhard Ebenhofer, Martin Melik Merkumians
  *    - initial API and implementation and/or initial documentation
+ *   Martin Erich Jobst
+ *    - add generic event accessors
  *******************************************************************************/
 #ifndef _GEN_PUBLISH_H_
 #define _GEN_PUBLISH_H_
@@ -19,11 +23,31 @@
  */
 
 class GEN_PUBLISH : public forte::com_infra::CCommFB {
-DECLARE_GENERIC_FIRMWARE_FB(GEN_PUBLISH)
+  DECLARE_GENERIC_FIRMWARE_FB(GEN_PUBLISH)
 
-public:
-  GEN_PUBLISH(const CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer);
-  ~GEN_PUBLISH() override = default;
+  public:
+    GEN_PUBLISH(const CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer);
+
+    ~GEN_PUBLISH() override = default;
+
+    template<typename ...Args>
+    void evt_INIT(Args&& ...paArgs) {
+      writeInputArguments(std::forward<Args>(paArgs)...);
+      receiveInputEvent(scmEventINITID, nullptr);
+      readOutputArguments(std::forward<Args>(paArgs)...);
+    }
+
+    template<typename ...Args>
+    void evt_REQ(Args&& ...paArgs) {
+      writeInputArguments(std::forward<Args>(paArgs)...);
+      receiveInputEvent(scmSendNotificationEventID, nullptr);
+      readOutputArguments(std::forward<Args>(paArgs)...);
+    }
+
+    template<typename ...Args>
+    void operator()(Args&& ...paArgs) {
+      evt_INIT(std::forward<Args>(paArgs)...);
+    }
 };
 
 #endif //_GEN_PUBLISH_H_
