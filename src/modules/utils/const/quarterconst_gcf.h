fba783756b793db72cdb8d76d91218bb57e51d27
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2024-10-02 19:09:59 +0200

    Add assembling and splitting modules to CMakeLists.
    - Added assembling module with various functions.
    - Added splitting module with corresponding functions.
    
    .

---
+++ src/modules/utils/const/quarterconst_gcf.h
@@ -0,0 +1,127 @@
+/*************************************************************************
+ *** Copyright (c) 2024 HR Agrartechnik GmbH
+ *** This program and the accompanying materials are made available under the
+ *** terms of the Eclipse Public License 2.0 which is available at
+ *** http://www.eclipse.org/legal/epl-2.0.
+ ***
+ *** SPDX-License-Identifier: EPL-2.0
+ ***
+ *** FORTE Language Element
+ ***
+ *** This file was generated using the 4DIAC FORTE Export Filter V1.0.x NG!
+ ***
+ *** Name: quarterconst_gcf
+ *************************************************************************/
+
+#ifndef _QUARTERCONST_GCF_H_
+#define _QUARTERCONST_GCF_H_
+
+#include "forte_byte.h"
+#include "forte_dword.h"
+#include "forte_lword.h"
+#include "forte_sint.h"
+#include "forte_word.h"
+#include "iec61131_functions.h"
+#include "forte_array_common.h"
+#include "forte_array.h"
+#include "forte_array_fixed.h"
+#include "forte_array_variable.h"
+
+extern const CIEC_BYTE st_global_BYTE_QUARTER_ZZ;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_00;
+extern const CIEC_BYTE st_global_BYTE_QUARTER_00;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_01;
+extern const CIEC_BYTE st_global_BYTE_QUARTER_01;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_02;
+extern const CIEC_BYTE st_global_BYTE_QUARTER_02;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_03;
+extern const CIEC_BYTE st_global_BYTE_QUARTER_03;
+extern const CIEC_WORD st_global_WORD_QUARTER_ZZ;
+extern const CIEC_WORD st_global_WORD_QUARTER_00;
+extern const CIEC_WORD st_global_WORD_QUARTER_01;
+extern const CIEC_WORD st_global_WORD_QUARTER_02;
+extern const CIEC_WORD st_global_WORD_QUARTER_03;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_04;
+extern const CIEC_WORD st_global_WORD_QUARTER_04;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_05;
+extern const CIEC_WORD st_global_WORD_QUARTER_05;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_06;
+extern const CIEC_WORD st_global_WORD_QUARTER_06;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_07;
+extern const CIEC_WORD st_global_WORD_QUARTER_07;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_ZZ;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_00;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_01;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_02;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_03;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_04;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_05;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_06;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_07;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_08;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_08;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_09;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_09;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_10;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_10;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_11;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_11;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_12;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_12;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_13;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_13;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_14;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_14;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_15;
+extern const CIEC_DWORD st_global_DWORD_QUARTER_15;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_ZZ;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_00;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_01;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_02;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_03;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_04;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_05;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_06;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_07;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_08;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_09;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_10;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_11;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_12;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_13;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_14;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_15;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_16;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_16;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_17;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_17;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_18;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_18;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_19;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_19;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_20;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_20;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_21;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_21;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_22;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_22;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_23;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_23;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_24;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_24;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_25;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_25;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_26;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_26;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_27;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_27;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_28;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_28;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_29;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_29;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_30;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_30;
+extern const CIEC_SINT st_global_SHIFT_QUARTER_31;
+extern const CIEC_LWORD st_global_LWORD_QUARTER_31;
+
+#endif // _QUARTERCONST_GCF_H_
