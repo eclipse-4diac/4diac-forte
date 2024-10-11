d13a4d171c071fee8eabd98feb9d186d5ec1a17b
franz-hoepfinger-4diac <franz.hoepfinger.4diac@logibus.tech>
2024-10-11 09:13:34 +0200

    Add new functions for retrieving current time and monotonic time. (#253)

---
+++ src/modules/utils/timing/F_NOW_MONOTONIC_fct.h
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2024 HR Agrartechnik GmbH
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+
+ *** This file was generated using the 4DIAC FORTE Export Filter V1.0.x NG!
+ ***
+ *** Name: F_NOW_MONOTONIC
+ *** Description: return a clear monotonic time
+ *** Version:
+ ***     1.0: 2024-10-01/Franz Höpfinger - HR Agrartechnik GmbH    -
+ *************************************************************************/
+
+#pragma once
+
+#include "funcbloc.h"
+#include "forte_time.h"
+#include "iec61131_functions.h"
+#include "forte_array_common.h"
+#include "forte_array.h"
+#include "forte_array_fixed.h"
+#include "forte_array_variable.h"
+
+class FORTE_utils__timing__F_NOW_MONOTONIC final : public CFunctionBlock {
+  DECLARE_FIRMWARE_FB(FORTE_utils__timing__F_NOW_MONOTONIC)
+
+  private:
+    static const CStringDictionary::TStringId scmDataOutputNames[];
+    static const CStringDictionary::TStringId scmDataOutputTypeIds[];
+    static const TEventID scmEventREQID = 0;
+    static const TForteInt16 scmEIWithIndexes[];
+    static const CStringDictionary::TStringId scmEventInputNames[];
+    static const TEventID scmEventCNFID = 0;
+    static const TDataIOID scmEOWith[];
+    static const TForteInt16 scmEOWithIndexes[];
+    static const CStringDictionary::TStringId scmEventOutputNames[];
+
+    static const SFBInterfaceSpec scmFBInterfaceSpec;
+
+    void executeEvent(TEventID paEIID, CEventChainExecutionThread *const paECET) override;
+
+    void readInputData(TEventID paEIID) override;
+    void writeOutputData(TEventID paEIID) override;
+    void setInitialValues() override;
+
+  public:
+    FORTE_utils__timing__F_NOW_MONOTONIC(CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer);
+
+    CIEC_TIME var_;
+
+    CIEC_TIME var_conn_;
+
+    CEventConnection conn_CNF;
+
+    CDataConnection conn_;
+
+    CIEC_ANY *getDI(size_t) override;
+    CIEC_ANY *getDO(size_t) override;
+    CEventConnection *getEOConUnchecked(TPortId) override;
+    CDataConnection **getDIConUnchecked(TPortId) override;
+    CDataConnection *getDOConUnchecked(TPortId) override;
+
+    void evt_REQ(CIEC_TIME &pa) {
+      executeEvent(scmEventREQID, nullptr);
+      pa = var_;
+    }
+
+    void operator()(CIEC_TIME &pa) {
+      evt_REQ(pa);
+    }
+};
+
+CIEC_TIME func_F_NOW_MONOTONIC();
+
