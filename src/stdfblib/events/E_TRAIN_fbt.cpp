463f432274055904ca19fea0baf47d5b6fb9a31b
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2025-02-16 15:14:37 +0100

    Refactor event handling and data connections in E_TRAIN component
    - Update event handling methods for START and STOP events
    - Modify data connection initialization and access methods
    
    Update copyright year to 2025 for fortiss GmbH and HR Agrartechnik GmbH.
    
    This commit changes the copyright year in the source files to 2025 for fortiss GmbH and HR Agrartechnik GmbH.

--- src/stdfblib/events/E_TRAIN_fbt.cpp
+++ src/stdfblib/events/E_TRAIN_fbt.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 fortiss GmbH
+ * Copyright (c) 2017, 2025 fortiss GmbH, HR Agrartechnik GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -7,18 +7,27 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- * Contributors:
- *   Alois Zoitl
- *     - initial API and implementation and/or initial documentation
- *******************************************************************************/
+ *** FORTE Library Element
+ ***
+ *** This file was generated using the 4DIAC FORTE Export Filter V1.0.x NG!
+ ***
+ *** Name: E_TRAIN
+ *** Description: Generate of a finite train of events seperated by the time DT
+ *** Version:
+ ***     1.0: 2017-09-22/Alois Zoitl - fortiss GmbH - initial API and implementation and/or initial documentation
+ ***     1.1: 2025-02-15/Franz Höpfinger -  HR Agrartechnik GmbH   - Bugfix
+ *************************************************************************/
 
 #include "E_TRAIN_fbt.h"
 #ifdef FORTE_ENABLE_GENERATED_SOURCE_CPP
 #include "E_TRAIN_fbt_gen.cpp"
 #endif
 
-#include "criticalregion.h"
-#include "resource.h"
+#include "iec61131_functions.h"
+#include "forte_array_common.h"
+#include "forte_array.h"
+#include "forte_array_fixed.h"
+#include "forte_array_variable.h"
 
 DEFINE_FIRMWARE_FB(FORTE_E_TRAIN, g_nStringIdE_TRAIN)
 
@@ -26,17 +35,15 @@ const CStringDictionary::TStringId FORTE_E_TRAIN::scmDataInputNames[] = {g_nStri
 const CStringDictionary::TStringId FORTE_E_TRAIN::scmDataInputTypeIds[] = {g_nStringIdTIME, g_nStringIdUINT};
 const CStringDictionary::TStringId FORTE_E_TRAIN::scmDataOutputNames[] = {g_nStringIdCV};
 const CStringDictionary::TStringId FORTE_E_TRAIN::scmDataOutputTypeIds[] = {g_nStringIdUINT};
-const TDataIOID FORTE_E_TRAIN::scmEIWith[] = {0, scmWithListDelimiter, 0, scmWithListDelimiter};
-const TForteInt16 FORTE_E_TRAIN::scmEIWithIndexes[] = {0, 2};
+const TDataIOID FORTE_E_TRAIN::scmEIWith[] = {0, 1, scmWithListDelimiter};
+const TForteInt16 FORTE_E_TRAIN::scmEIWithIndexes[] = {0, -1};
 const CStringDictionary::TStringId FORTE_E_TRAIN::scmEventInputNames[] = {g_nStringIdSTART, g_nStringIdSTOP};
-const CStringDictionary::TStringId FORTE_E_TRAIN::scmEventInputTypeIds[] = {g_nStringIdEvent, g_nStringIdEvent};
 const TDataIOID FORTE_E_TRAIN::scmEOWith[] = {0, scmWithListDelimiter};
 const TForteInt16 FORTE_E_TRAIN::scmEOWithIndexes[] = {0};
 const CStringDictionary::TStringId FORTE_E_TRAIN::scmEventOutputNames[] = {g_nStringIdEO};
-const CStringDictionary::TStringId FORTE_E_TRAIN::scmEventOutputTypeIds[] = {g_nStringIdEvent};
 const SFBInterfaceSpec FORTE_E_TRAIN::scmFBInterfaceSpec = {
-  2, scmEventInputNames, scmEventInputTypeIds, scmEIWith, scmEIWithIndexes,
-  1, scmEventOutputNames, scmEventOutputTypeIds, scmEOWith, scmEOWithIndexes,
+  2, scmEventInputNames, nullptr, scmEIWith, scmEIWithIndexes,
+  1, scmEventOutputNames, nullptr, scmEOWith, scmEOWithIndexes,
   2, scmDataInputNames, scmDataInputTypeIds,
   1, scmDataOutputNames, scmDataOutputTypeIds,
   0, nullptr,
@@ -48,9 +55,9 @@ FORTE_E_TRAIN::FORTE_E_TRAIN(const CStringDictionary::TStringId paInstanceNameId
     fb_CTR(g_nStringIdCTR, *this),
     fb_GATE(g_nStringIdGATE, *this),
     fb_DLY(g_nStringIdDLY, *this),
-    var_DT(CIEC_TIME(0)),
-    var_N(CIEC_UINT(0)),
-    var_CV(CIEC_UINT(0)),
+    var_DT(0_TIME),
+    var_N(0_UINT),
+    var_CV(0_UINT),
     var_conn_CV(var_CV),
     conn_EO(this, 0),
     conn_DT(nullptr),
@@ -58,12 +65,17 @@ FORTE_E_TRAIN::FORTE_E_TRAIN(const CStringDictionary::TStringId paInstanceNameId
     conn_CV(this, 0, &var_conn_CV) {
 };
 
+void FORTE_E_TRAIN::setInitialValues() {
+  var_DT = 0_TIME;
+  var_N = 0_UINT;
+  var_CV = 0_UINT;
+}
+
 const SCFB_FBInstanceData FORTE_E_TRAIN::scmInternalFBs[] = {
   {g_nStringIdCTR, g_nStringIdE_CTU},
   {g_nStringIdGATE, g_nStringIdE_SWITCH},
   {g_nStringIdDLY, g_nStringIdE_DELAY}
 };
-
 
 const SCFB_FBConnectionData FORTE_E_TRAIN::scmEventConnections[] = {
   {GENERATE_CONNECTION_PORT_ID_2_ARG(g_nStringIdCTR, g_nStringIdCUO), 0, GENERATE_CONNECTION_PORT_ID_2_ARG(g_nStringIdGATE, g_nStringIdEI), 1},
@@ -91,19 +103,24 @@ const SCFB_FBNData FORTE_E_TRAIN::scmFBNData = {
   1, scmFannedOutEventConnections,
   4, scmDataConnections,
   0, nullptr,
-  0, nullptr,
   0, nullptr
 };
 
-
-void FORTE_E_TRAIN::readInputData(TEventID paEIID) {
+void FORTE_E_TRAIN::readInternal2InterfaceOutputData(const TEventID paEOID) {
+  switch(paEOID) {
+    case scmEventEOID: {
+      if(CDataConnection *conn = getIn2IfConUnchecked(0); conn) { conn->readData(var_CV); }
+      break;
+    }
+    default:
+      break;
+  }
+}
+void FORTE_E_TRAIN::readInputData(const TEventID paEIID) {
   switch(paEIID) {
     case scmEventSTARTID: {
       readData(0, var_DT, conn_DT);
-      break;
-    }
-    case scmEventSTOPID: {
-      readData(0, var_DT, conn_DT);
+      readData(1, var_N, conn_N);
       break;
     }
     default:
@@ -111,7 +128,7 @@ void FORTE_E_TRAIN::readInputData(TEventID paEIID) {
   }
 }
 
-void FORTE_E_TRAIN::writeOutputData(TEventID paEIID) {
+void FORTE_E_TRAIN::writeOutputData(const TEventID paEIID) {
   switch(paEIID) {
     case scmEventEOID: {
       writeData(0, var_CV, conn_CV);
@@ -122,18 +139,7 @@ void FORTE_E_TRAIN::writeOutputData(TEventID paEIID) {
   }
 }
 
-void FORTE_E_TRAIN::readInternal2InterfaceOutputData(TEventID paEOID) {
-  switch(paEOID) {
-    case scmEventEOID: {
-      if(CDataConnection *conn = getIn2IfConUnchecked(0); conn) { conn->readData(var_CV); }
-      break;
-    }
-    default:
-      break;
-  }
-}
-
-CIEC_ANY *FORTE_E_TRAIN::getDI(size_t paIndex) {
+CIEC_ANY *FORTE_E_TRAIN::getDI(const size_t paIndex) {
   switch(paIndex) {
     case 0: return &var_DT;
     case 1: return &var_N;
@@ -141,21 +147,21 @@ CIEC_ANY *FORTE_E_TRAIN::getDI(size_t paIndex) {
   return nullptr;
 }
 
-CIEC_ANY *FORTE_E_TRAIN::getDO(size_t paIndex) {
+CIEC_ANY *FORTE_E_TRAIN::getDO(const size_t paIndex) {
   switch(paIndex) {
     case 0: return &var_CV;
   }
   return nullptr;
 }
 
-CEventConnection *FORTE_E_TRAIN::getEOConUnchecked(TPortId paIndex) {
+CEventConnection *FORTE_E_TRAIN::getEOConUnchecked(const TPortId paIndex) {
   switch(paIndex) {
     case 0: return &conn_EO;
   }
   return nullptr;
 }
 
-CDataConnection **FORTE_E_TRAIN::getDIConUnchecked(TPortId paIndex) {
+CDataConnection **FORTE_E_TRAIN::getDIConUnchecked(const TPortId paIndex) {
   switch(paIndex) {
     case 0: return &conn_DT;
     case 1: return &conn_N;
@@ -163,11 +169,9 @@ CDataConnection **FORTE_E_TRAIN::getDIConUnchecked(TPortId paIndex) {
   return nullptr;
 }
 
-CDataConnection *FORTE_E_TRAIN::getDOConUnchecked(TPortId paIndex) {
+CDataConnection *FORTE_E_TRAIN::getDOConUnchecked(const TPortId paIndex) {
   switch(paIndex) {
     case 0: return &conn_CV;
   }
   return nullptr;
 }
-
-
--- src/stdfblib/events/E_TRAIN_fbt.h
+++ src/stdfblib/events/E_TRAIN_fbt.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 fortiss GmbH
+ * Copyright (c) 2017, 2025 fortiss GmbH, HR Agrartechnik GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -7,15 +7,22 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- * Contributors:
- *   Alois Zoitl
- *     - initial API and implementation and/or initial documentation
- *******************************************************************************/
+ *** FORTE Library Element
+ ***
+ *** This file was generated using the 4DIAC FORTE Export Filter V1.0.x NG!
+ ***
+ *** Name: E_TRAIN
+ *** Description: Generate of a finite train of events seperated by the time DT
+ *** Version:
+ ***     1.0: 2017-09-22/Alois Zoitl - fortiss GmbH - initial API and implementation and/or initial documentation
+ ***     1.1: 2025-02-15/Franz Höpfinger -  HR Agrartechnik GmbH   - Bugfix
+ *************************************************************************/
 
 #pragma once
 
 #include "cfb.h"
 #include "typelib.h"
+#include "forte_bool.h"
 #include "forte_time.h"
 #include "forte_uint.h"
 #include "iec61131_functions.h"
@@ -24,80 +31,84 @@
 #include "forte_array_fixed.h"
 #include "forte_array_variable.h"
 #include "E_CTU_fbt.h"
-#include "E_SWITCH_fbt.h"
 #include "E_DELAY_fbt.h"
+#include "E_SWITCH_fbt.h"
 
-
-class FORTE_E_TRAIN: public CCompositeFB {
+class FORTE_E_TRAIN final : public CCompositeFB {
   DECLARE_FIRMWARE_FB(FORTE_E_TRAIN)
 
-private:
-  static const CStringDictionary::TStringId scmDataInputNames[];
-  static const CStringDictionary::TStringId scmDataInputTypeIds[];
-  static const CStringDictionary::TStringId scmDataOutputNames[];
-  static const CStringDictionary::TStringId scmDataOutputTypeIds[];
-  static const TEventID scmEventSTARTID = 0;
-  static const TEventID scmEventSTOPID = 1;
-  static const TDataIOID scmEIWith[];
-  static const TForteInt16 scmEIWithIndexes[];
-  static const CStringDictionary::TStringId scmEventInputNames[];
-  static const CStringDictionary::TStringId scmEventInputTypeIds[];
-  static const TEventID scmEventEOID = 0;
-  static const TDataIOID scmEOWith[];
-  static const TForteInt16 scmEOWithIndexes[];
-  static const CStringDictionary::TStringId scmEventOutputNames[];
-  static const CStringDictionary::TStringId scmEventOutputTypeIds[];
+  private:
+    static const CStringDictionary::TStringId scmDataInputNames[];
+    static const CStringDictionary::TStringId scmDataInputTypeIds[];
+    static const CStringDictionary::TStringId scmDataOutputNames[];
+    static const CStringDictionary::TStringId scmDataOutputTypeIds[];
+    static const TEventID scmEventSTARTID = 0;
+    static const TEventID scmEventSTOPID = 1;
+    static const TDataIOID scmEIWith[];
+    static const TForteInt16 scmEIWithIndexes[];
+    static const CStringDictionary::TStringId scmEventInputNames[];
+    static const TEventID scmEventEOID = 0;
+    static const TDataIOID scmEOWith[];
+    static const TForteInt16 scmEOWithIndexes[];
+    static const CStringDictionary::TStringId scmEventOutputNames[];
 
-  static const SFBInterfaceSpec scmFBInterfaceSpec;
+    static const SFBInterfaceSpec scmFBInterfaceSpec;
 
-  static const SCFB_FBInstanceData scmInternalFBs[];
-  static const SCFB_FBParameter scmParamters[];
-  static const SCFB_FBConnectionData scmEventConnections[];
-  static const SCFB_FBFannedOutConnectionData scmFannedOutEventConnections[];
-  static const SCFB_FBConnectionData scmDataConnections[];
-  static const SCFB_FBFannedOutConnectionData scmFannedOutDataConnections[];
-  static const SCFB_FBNData scmFBNData;
+    static const SCFB_FBInstanceData scmInternalFBs[];
+    static const SCFB_FBConnectionData scmEventConnections[];
+    static const SCFB_FBFannedOutConnectionData scmFannedOutEventConnections[];
+    static const SCFB_FBConnectionData scmDataConnections[];
+    static const SCFB_FBFannedOutConnectionData scmFannedOutDataConnections[];
+    static const SCFB_FBNData scmFBNData;
 
-  forte::core::CInternalFB<FORTE_E_CTU> fb_CTR;
-  forte::core::CInternalFB<FORTE_E_SWITCH> fb_GATE;
-  forte::core::CInternalFB<FORTE_E_DELAY> fb_DLY;
+    forte::core::CInternalFB<FORTE_E_CTU> fb_CTR;
+    forte::core::CInternalFB<FORTE_E_SWITCH> fb_GATE;
+    forte::core::CInternalFB<FORTE_E_DELAY> fb_DLY;
 
-  void readInputData(TEventID paEIID) override;
-  void writeOutputData(TEventID paEIID) override;
-  void readInternal2InterfaceOutputData(TEventID paEOID) override;
+    void readInputData(TEventID paEIID) override;
+    void writeOutputData(TEventID paEIID) override;
+    void readInternal2InterfaceOutputData(TEventID paEOID) override;
+    void setInitialValues() override;
 
-public:
-  FORTE_E_TRAIN(const CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer);
+  public:
+    FORTE_E_TRAIN(CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer);
 
-  CIEC_TIME var_DT;
-  CIEC_UINT var_N;
-  CIEC_UINT var_CV;
-  CIEC_UINT var_conn_CV;
-  CEventConnection conn_EO;
-  CDataConnection *conn_DT;
-  CDataConnection *conn_N;
-  CDataConnection conn_CV;
-  CIEC_ANY *getDI(size_t) override;
-  CIEC_ANY *getDO(size_t) override;
-  CEventConnection *getEOConUnchecked(TPortId) override;
-  CDataConnection **getDIConUnchecked(TPortId) override;
-  CDataConnection *getDOConUnchecked(TPortId) override;
-  void evt_START(const CIEC_TIME &pa_DT, const CIEC_UINT &pa_N, CIEC_UINT &pa_CV) {
-    var_DT = pa_DT;
-    var_N = pa_N;
-    receiveInputEvent(scmEventSTARTID, nullptr);
-    pa_CV = var_CV;
-  }
-  void evt_STOP(const CIEC_TIME &pa_DT, const CIEC_UINT &pa_N, CIEC_UINT &pa_CV) {
-    var_DT = pa_DT;
-    var_N = pa_N;
-    receiveInputEvent(scmEventSTOPID, nullptr);
-    pa_CV = var_CV;
-  }
-  void operator()(const CIEC_TIME &pa_DT, const CIEC_UINT &pa_N, CIEC_UINT &pa_CV) {
-    evt_START(pa_DT, pa_N, pa_CV);
-  }
+    CIEC_TIME var_DT;
+    CIEC_UINT var_N;
+
+    CIEC_UINT var_CV;
+
+    CIEC_UINT var_conn_CV;
+
+    CEventConnection conn_EO;
+
+    CDataConnection *conn_DT;
+    CDataConnection *conn_N;
+
+    CDataConnection conn_CV;
+
+    CIEC_ANY *getDI(size_t) override;
+    CIEC_ANY *getDO(size_t) override;
+    CEventConnection *getEOConUnchecked(TPortId) override;
+    CDataConnection **getDIConUnchecked(TPortId) override;
+    CDataConnection *getDOConUnchecked(TPortId) override;
+
+    void evt_START(const CIEC_TIME &paDT, const CIEC_UINT &paN, CIEC_UINT &paCV) {
+      var_DT = paDT;
+      var_N = paN;
+      executeEvent(scmEventSTARTID, nullptr);
+      paCV = var_CV;
+    }
+
+    void evt_STOP(const CIEC_TIME &paDT, const CIEC_UINT &paN, CIEC_UINT &paCV) {
+      var_DT = paDT;
+      var_N = paN;
+      executeEvent(scmEventSTOPID, nullptr);
+      paCV = var_CV;
+    }
+
+    void operator()(const CIEC_TIME &paDT, const CIEC_UINT &paN, CIEC_UINT &paCV) {
+      evt_START(paDT, paN, paCV);
+    }
 };
-
-
 
