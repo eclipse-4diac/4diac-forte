5bd79db6d18728e1b910befd75494aafd37f2c66
Moritz Ortmeier <moritz.ortmeier@logibus.tech>
2025-03-07 18:12:39 +0100

    Generated PLC01A1 from IDE to also use EInit, initail commit
    merged the generated PLC01A1 with the plc01a1_config file

--- src/modules/PLC01A1/plc01a1_config_fb.cpp
+++ src/modules/PLC01A1/plc01a1_config_fb.cpp
@@ -15,10 +15,15 @@
 #include "plc01a1_config_fb_gen.cpp"
 #endif
 
+#include "forte_uint.h"
+#include "iec61131_functions.h"
+#include "forte_array_common.h"
+#include "forte_array.h"
+#include "forte_array_fixed.h"
+#include "forte_array_variable.h"
 #include "plc01a1_controller.h"
 
 DEFINE_FIRMWARE_FB(PLC01A1ConfigFB, g_nStringIdPLC01A1)
-
 
 
 const CStringDictionary::TStringId PLC01A1ConfigFB::scmDataInputNames[] = { g_nStringIdQI, g_nStringIdIN1, g_nStringIdIN2, g_nStringIdIN3, g_nStringIdIN4,
@@ -30,11 +35,10 @@ const CStringDictionary::TStringId PLC01A1ConfigFB::scmDataInputTypeIds[] = { g_
   g_nStringIdWSTRING, g_nStringIdWSTRING, g_nStringIdWSTRING, g_nStringIdWSTRING, g_nStringIdWSTRING, g_nStringIdWSTRING, g_nStringIdUINT };
 
 const CStringDictionary::TStringId PLC01A1ConfigFB::scmDataOutputNames[] = { g_nStringIdQO, g_nStringIdSTATUS };
-
 const CStringDictionary::TStringId PLC01A1ConfigFB::scmDataOutputTypeIds[] = { g_nStringIdBOOL, g_nStringIdWSTRING };
-
 const TForteInt16 PLC01A1ConfigFB::scmEIWithIndexes[] = { 0 };
 const TDataIOID PLC01A1ConfigFB::scmEIWith[] = { 0, 17, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, scmWithListDelimiter };
+const TForteInt16 PLC01A1ConfigFB::scmEIWithIndexes[] = {0};
 const CStringDictionary::TStringId PLC01A1ConfigFB::scmEventInputNames[] = { g_nStringIdINIT };
 const CStringDictionary::TStringId PLC01A1ConfigFB::scmEventInputTypeIds[] = {g_nStringIdEInit};
 const TDataIOID PLC01A1ConfigFB::scmEOWith[] = { 0, 1, scmWithListDelimiter, 0, 1, scmWithListDelimiter };
@@ -44,12 +48,90 @@ const CStringDictionary::TStringId PLC01A1ConfigFB::scmEventOutputTypeIds[] = {g
 const SFBInterfaceSpec PLC01A1ConfigFB::scmFBInterfaceSpec = {
   1,  scmEventInputNames, scmEventInputTypeIds,  scmEIWith,  scmEIWithIndexes,
   2,  scmEventOutputNames, scmEventOutputTypeIds,  scmEOWith, scmEOWithIndexes,  18,  scmDataInputNames, scmDataInputTypeIds,
-  2,  scmDataOutputNames, scmDataOutputTypeIds,
-  0, 0
+  18, scmDataInputNames, scmDataInputTypeIds,
+  2, scmDataOutputNames, scmDataOutputTypeIds,
+  0, nullptr,
+  0, nullptr
+};
+
+PLC01A1ConfigFB::PLC01A1ConfigFB(const CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer) :
+    CFunctionBlock(paContainer, scmFBInterfaceSpec, paInstanceNameId),
+    var_QI(0_BOOL),
+    var_IN1(u""_WSTRING),
+    var_IN2(u""_WSTRING),
+    var_IN3(u""_WSTRING),
+    var_IN4(u""_WSTRING),
+    var_IN5(u""_WSTRING),
+    var_IN6(u""_WSTRING),
+    var_IN7(u""_WSTRING),
+    var_IN8(u""_WSTRING),
+    var_OUT1(u""_WSTRING),
+    var_OUT2(u""_WSTRING),
+    var_OUT3(u""_WSTRING),
+    var_OUT4(u""_WSTRING),
+    var_OUT5(u""_WSTRING),
+    var_OUT6(u""_WSTRING),
+    var_OUT7(u""_WSTRING),
+    var_OUT8(u""_WSTRING),
+    var_UpdateInterval(25_UINT),
+    var_QO(0_BOOL),
+    var_STATUS(u""_WSTRING),
+    var_conn_QO(var_QO),
+    var_conn_STATUS(var_STATUS),
+    conn_INITO(this, 0),
+    conn_IND(this, 1),
+    conn_QI(nullptr),
+    conn_IN1(nullptr),
+    conn_IN2(nullptr),
+    conn_IN3(nullptr),
+    conn_IN4(nullptr),
+    conn_IN5(nullptr),
+    conn_IN6(nullptr),
+    conn_IN7(nullptr),
+    conn_IN8(nullptr),
+    conn_OUT1(nullptr),
+    conn_OUT2(nullptr),
+    conn_OUT3(nullptr),
+    conn_OUT4(nullptr),
+    conn_OUT5(nullptr),
+    conn_OUT6(nullptr),
+    conn_OUT7(nullptr),
+    conn_OUT8(nullptr),
+    conn_UpdateInterval(nullptr),
+    conn_QO(this, 0, &var_conn_QO),
+    conn_STATUS(this, 1, &var_conn_STATUS) {
 };
 
 void PLC01A1ConfigFB::setInitialValues() {
-  UpdateInterval() = 25;
+  var_QI = 0_BOOL;
+  var_IN1 = u""_WSTRING;
+  var_IN2 = u""_WSTRING;
+  var_IN3 = u""_WSTRING;
+  var_IN4 = u""_WSTRING;
+  var_IN5 = u""_WSTRING;
+  var_IN6 = u""_WSTRING;
+  var_IN7 = u""_WSTRING;
+  var_IN8 = u""_WSTRING;
+  var_OUT1 = u""_WSTRING;
+  var_OUT2 = u""_WSTRING;
+  var_OUT3 = u""_WSTRING;
+  var_OUT4 = u""_WSTRING;
+  var_OUT5 = u""_WSTRING;
+  var_OUT6 = u""_WSTRING;
+  var_OUT7 = u""_WSTRING;
+  var_OUT8 = u""_WSTRING;
+  var_UpdateInterval = 25_UINT;
+  var_QO = 0_BOOL;
+  var_STATUS = u""_WSTRING;
+}
+
+void PLC01A1ConfigFB::executeEvent(const TEventID paEIID, CEventChainExecutionThread *const paECET) {
+  switch(paEIID) {
+    case scmEventINITID:
+      #error add code for INIT event!
+          sendOutputEvent(scmEventCNFID, paECET);
+      break;
+  }
 }
 
 forte::core::io::IODeviceController* PLC01A1ConfigFB::createDeviceController(CDeviceExecution &paDeviceExecution) {
@@ -86,5 +168,119 @@ void PLC01A1ConfigFB::onStartup() {
   started();
 }
 
+void PLC01A1ConfigFB::readInputData(const TEventID paEIID) {
+  switch(paEIID) {
+    case scmEventINITID: {
+      readData(0, var_QI, conn_QI);
+      readData(17, var_UpdateInterval, conn_UpdateInterval);
+      readData(1, var_IN1, conn_IN1);
+      readData(2, var_IN2, conn_IN2);
+      readData(3, var_IN3, conn_IN3);
+      readData(4, var_IN4, conn_IN4);
+      readData(5, var_IN5, conn_IN5);
+      readData(6, var_IN6, conn_IN6);
+      readData(7, var_IN7, conn_IN7);
+      readData(8, var_IN8, conn_IN8);
+      readData(9, var_OUT1, conn_OUT1);
+      readData(10, var_OUT2, conn_OUT2);
+      readData(11, var_OUT3, conn_OUT3);
+      readData(12, var_OUT4, conn_OUT4);
+      readData(13, var_OUT5, conn_OUT5);
+      readData(14, var_OUT6, conn_OUT6);
+      readData(15, var_OUT7, conn_OUT7);
+      readData(16, var_OUT8, conn_OUT8);
+      break;
+    }
+    default:
+      break;
+  }
+}
 
+void PLC01A1ConfigFB::writeOutputData(const TEventID paEIID) {
+  switch(paEIID) {
+    case scmEventINITOID: {
+      writeData(0, var_QO, conn_QO);
+      writeData(1, var_STATUS, conn_STATUS);
+      break;
+    }
+    case scmEventINDID: {
+      writeData(0, var_QO, conn_QO);
+      writeData(1, var_STATUS, conn_STATUS);
+      break;
+    }
+    default:
+      break;
+  }
+}
 
+CIEC_ANY *PLC01A1ConfigFB::getDI(const size_t paIndex) {
+  switch(paIndex) {
+    case 0: return &var_QI;
+    case 1: return &var_IN1;
+    case 2: return &var_IN2;
+    case 3: return &var_IN3;
+    case 4: return &var_IN4;
+    case 5: return &var_IN5;
+    case 6: return &var_IN6;
+    case 7: return &var_IN7;
+    case 8: return &var_IN8;
+    case 9: return &var_OUT1;
+    case 10: return &var_OUT2;
+    case 11: return &var_OUT3;
+    case 12: return &var_OUT4;
+    case 13: return &var_OUT5;
+    case 14: return &var_OUT6;
+    case 15: return &var_OUT7;
+    case 16: return &var_OUT8;
+    case 17: return &var_UpdateInterval;
+  }
+  return nullptr;
+}
+
+CIEC_ANY *PLC01A1ConfigFB::getDO(const size_t paIndex) {
+  switch(paIndex) {
+    case 0: return &var_QO;
+    case 1: return &var_STATUS;
+  }
+  return nullptr;
+}
+
+CEventConnection *PLC01A1ConfigFB::getEOConUnchecked(const TPortId paIndex) {
+  switch(paIndex) {
+    case 0: return &conn_INITO;
+    case 1: return &conn_IND;
+  }
+  return nullptr;
+}
+
+CDataConnection **PLC01A1ConfigFB::getDIConUnchecked(const TPortId paIndex) {
+  switch(paIndex) {
+    case 0: return &conn_QI;
+    case 1: return &conn_IN1;
+    case 2: return &conn_IN2;
+    case 3: return &conn_IN3;
+    case 4: return &conn_IN4;
+    case 5: return &conn_IN5;
+    case 6: return &conn_IN6;
+    case 7: return &conn_IN7;
+    case 8: return &conn_IN8;
+    case 9: return &conn_OUT1;
+    case 10: return &conn_OUT2;
+    case 11: return &conn_OUT3;
+    case 12: return &conn_OUT4;
+    case 13: return &conn_OUT5;
+    case 14: return &conn_OUT6;
+    case 15: return &conn_OUT7;
+    case 16: return &conn_OUT8;
+    case 17: return &conn_UpdateInterval;
+  }
+  return nullptr;
+}
+
+CDataConnection *PLC01A1ConfigFB::getDOConUnchecked(const TPortId paIndex) {
+  switch(paIndex) {
+    case 0: return &conn_QO;
+    case 1: return &conn_STATUS;
+  }
+  return nullptr;
+}
--- src/modules/PLC01A1/plc01a1_config_fb.h
+++ src/modules/PLC01A1/plc01a1_config_fb.h
@@ -10,21 +10,26 @@
  *   Jose Cabral - initial API and implementation and/or initial documentation
  *******************************************************************************/
 
-#ifndef _PLC01A1_H_
+#pragma once
 #define _PLC01A1_H_
 
-#include <funcbloc.h>
-#include <forte_bool.h>
-#include <forte_uint.h>
-#include <forte_wstring.h>
+#include "funcbloc.h"
+#include "forte_bool.h"
+#include "forte_uint.h"
+#include "forte_wstring.h"
+#include "iec61131_functions.h"
+#include "forte_array_common.h"
+#include "forte_array.h"
+#include "forte_array_fixed.h"
+#include "forte_array_variable.h"
 #include <io/configFB/io_configFB_controller.h>
 
 class PLC01A1ConfigFB : public forte::core::io::IOConfigFBController {
   DECLARE_FIRMWARE_FB(PLC01A1ConfigFB)
 
-private:
-  static const CStringDictionary::TStringId scmDataInputNames[];
-  static const CStringDictionary::TStringId scmDataInputTypeIds[];
+  private:
+    static const CStringDictionary::TStringId scmDataInputNames[];
+    static const CStringDictionary::TStringId scmDataInputTypeIds[];
   CIEC_BOOL &QI() {
     return *static_cast<CIEC_BOOL*>(getDI(0));
   };
@@ -96,9 +101,8 @@ private:
   CIEC_UINT &UpdateInterval() {
     return *static_cast<CIEC_UINT*>(getDI(17));
   };
-
-  static const CStringDictionary::TStringId scmDataOutputNames[];
-  static const CStringDictionary::TStringId scmDataOutputTypeIds[];
+    static const CStringDictionary::TStringId scmDataOutputNames[];
+    static const CStringDictionary::TStringId scmDataOutputTypeIds[];
   CIEC_BOOL &QO() {
     return *static_cast<CIEC_BOOL*>(getDO(0));
   };
@@ -106,21 +110,19 @@ private:
   CIEC_WSTRING &STATUS() {
     return *static_cast<CIEC_WSTRING*>(getDO(1));
   };
+    static const TEventID scmEventINITID = 0;
+    static const TDataIOID scmEIWith[];
+    static const TForteInt16 scmEIWithIndexes[];
+    static const CStringDictionary::TStringId scmEventInputNames[];
+    static const CStringDictionary::TStringId scmEventInputTypeIds[];
+    static const TEventID scmEventINITOID = 0;
+    static const TEventID scmEventINDID = 1;
+    static const TDataIOID scmEOWith[];
+    static const TForteInt16 scmEOWithIndexes[];
+    static const CStringDictionary::TStringId scmEventOutputNames[];
+    static const CStringDictionary::TStringId scmEventOutputTypeIds[];
 
-  static const TEventID scmEventINITID = 0;
-  static const TForteInt16 scmEIWithIndexes[];
-  static const TDataIOID scmEIWith[];
-  static const CStringDictionary::TStringId scmEventInputNames[];
-  static const CStringDictionary::TStringId scmEventInputTypeIds[];
-
-  static const TEventID scmEventINITOID = 0;
-  static const TEventID scmEventINDID = 1;
-  static const TForteInt16 scmEOWithIndexes[];
-  static const TDataIOID scmEOWith[];
-  static const CStringDictionary::TStringId scmEventOutputNames[];
-  static const CStringDictionary::TStringId scmEventOutputTypeIds[];
-
-  static const SFBInterfaceSpec scmFBInterfaceSpec;
+    static const SFBInterfaceSpec scmFBInterfaceSpec;
 
 
 virtual void setInitialValues();
@@ -131,11 +133,97 @@ virtual void setInitialValues();
     void setConfig();
 
     virtual void onStartup();
+    void executeEvent(TEventID paEIID, CEventChainExecutionThread *const paECET) override;
+
+    void readInputData(TEventID paEIID) override;
+    void writeOutputData(TEventID paEIID) override;
+    void setInitialValues() override;
 
   public:
     FUNCTION_BLOCK_CTOR_WITH_BASE_CLASS(PLC01A1ConfigFB, forte::core::io::IOConfigFBController){}
 
-};
+    CIEC_BOOL var_QI;
+    CIEC_WSTRING var_IN1;
+    CIEC_WSTRING var_IN2;
+    CIEC_WSTRING var_IN3;
+    CIEC_WSTRING var_IN4;
+    CIEC_WSTRING var_IN5;
+    CIEC_WSTRING var_IN6;
+    CIEC_WSTRING var_IN7;
+    CIEC_WSTRING var_IN8;
+    CIEC_WSTRING var_OUT1;
+    CIEC_WSTRING var_OUT2;
+    CIEC_WSTRING var_OUT3;
+    CIEC_WSTRING var_OUT4;
+    CIEC_WSTRING var_OUT5;
+    CIEC_WSTRING var_OUT6;
+    CIEC_WSTRING var_OUT7;
+    CIEC_WSTRING var_OUT8;
+    CIEC_UINT var_UpdateInterval;
 
-#endif //close the ifdef sequence from the beginning of the file
+    CIEC_BOOL var_QO;
+    CIEC_WSTRING var_STATUS;
+
+    CIEC_BOOL var_conn_QO;
+    CIEC_WSTRING var_conn_STATUS;
+
+    CEventConnection conn_INITO;
+    CEventConnection conn_IND;
+
+    CDataConnection *conn_QI;
+    CDataConnection *conn_IN1;
+    CDataConnection *conn_IN2;
+    CDataConnection *conn_IN3;
+    CDataConnection *conn_IN4;
+    CDataConnection *conn_IN5;
+    CDataConnection *conn_IN6;
+    CDataConnection *conn_IN7;
+    CDataConnection *conn_IN8;
+    CDataConnection *conn_OUT1;
+    CDataConnection *conn_OUT2;
+    CDataConnection *conn_OUT3;
+    CDataConnection *conn_OUT4;
+    CDataConnection *conn_OUT5;
+    CDataConnection *conn_OUT6;
+    CDataConnection *conn_OUT7;
+    CDataConnection *conn_OUT8;
+    CDataConnection *conn_UpdateInterval;
+
+    CDataConnection conn_QO;
+    CDataConnection conn_STATUS;
+
+    CIEC_ANY *getDI(size_t) override;
+    CIEC_ANY *getDO(size_t) override;
+    CEventConnection *getEOConUnchecked(TPortId) override;
+    CDataConnection **getDIConUnchecked(TPortId) override;
+    CDataConnection *getDOConUnchecked(TPortId) override;
+
+    void evt_INIT(const CIEC_BOOL &paQI, const CIEC_WSTRING &paIN1, const CIEC_WSTRING &paIN2, const CIEC_WSTRING &paIN3, const CIEC_WSTRING &paIN4, const CIEC_WSTRING &paIN5, const CIEC_WSTRING &paIN6, const CIEC_WSTRING &paIN7, const CIEC_WSTRING &paIN8, const CIEC_WSTRING &paOUT1, const CIEC_WSTRING &paOUT2, const CIEC_WSTRING &paOUT3, const CIEC_WSTRING &paOUT4, const CIEC_WSTRING &paOUT5, const CIEC_WSTRING &paOUT6, const CIEC_WSTRING &paOUT7, const CIEC_WSTRING &paOUT8, const CIEC_UINT &paUpdateInterval, CIEC_BOOL &paQO, CIEC_WSTRING &paSTATUS) {
+      var_QI = paQI;
+      var_IN1 = paIN1;
+      var_IN2 = paIN2;
+      var_IN3 = paIN3;
+      var_IN4 = paIN4;
+      var_IN5 = paIN5;
+      var_IN6 = paIN6;
+      var_IN7 = paIN7;
+      var_IN8 = paIN8;
+      var_OUT1 = paOUT1;
+      var_OUT2 = paOUT2;
+      var_OUT3 = paOUT3;
+      var_OUT4 = paOUT4;
+      var_OUT5 = paOUT5;
+      var_OUT6 = paOUT6;
+      var_OUT7 = paOUT7;
+      var_OUT8 = paOUT8;
+      var_UpdateInterval = paUpdateInterval;
+      executeEvent(scmEventINITID, nullptr);
+      paQO = var_QO;
+      paSTATUS = var_STATUS;
+    }
+
+    void operator()(const CIEC_BOOL &paQI, const CIEC_WSTRING &paIN1, const CIEC_WSTRING &paIN2, const CIEC_WSTRING &paIN3, const CIEC_WSTRING &paIN4, const CIEC_WSTRING &paIN5, const CIEC_WSTRING &paIN6, const CIEC_WSTRING &paIN7, const CIEC_WSTRING &paIN8, const CIEC_WSTRING &paOUT1, const CIEC_WSTRING &paOUT2, const CIEC_WSTRING &paOUT3, const CIEC_WSTRING &paOUT4, const CIEC_WSTRING &paOUT5, const CIEC_WSTRING &paOUT6, const CIEC_WSTRING &paOUT7, const CIEC_WSTRING &paOUT8, const CIEC_UINT &paUpdateInterval, CIEC_BOOL &paQO, CIEC_WSTRING &paSTATUS) {
+      evt_INIT(paQI, paIN1, paIN2, paIN3, paIN4, paIN5, paIN6, paIN7, paIN8, paOUT1, paOUT2, paOUT3, paOUT4, paOUT5, paOUT6, paOUT7, paOUT8, paUpdateInterval, paQO, paSTATUS);
+    }
+};
 
