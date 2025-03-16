ae17a21257948e690854cef92a8f48b93bb12a59
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2025-03-16 01:32:43 +0100

    Update copyright info with new contributor
    - Added HR Agrartechnik GmbH to the copyright section
    - Updated both source and header files for consistency

--- src/core/io/processinterface.cpp
+++ src/core/io/processinterface.cpp
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2016, 2025 Johannes Messmer (admin@jomess.com), fortiss GmbH,
  *                          Johannes Kepler University Linz, Thomas Öllinger
+ *                          HR Agrartechnik GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
