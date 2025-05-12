6fc5e3fdb66b39a53d56134abd00d39e48ebc3fc
Martin Jobst <martin.jobst@posteo.de>
2025-05-12 13:20:30 +0200

    Add support for infinity and nan values in toString of (L)REAL
    Any integer (L)REAL values require a decimal point to be added when
    converting to a string. This did not account for infinity and nan values
    and erroneously added a decimal point after those values, as well.
    
    This changes the normalization function to account for infinity and nan
    values.

--- src/core/datatypes/forte_any_real.cpp
+++ src/core/datatypes/forte_any_real.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 ACIN
+ * Copyright (c) 2011, 2025 ACIN, Martin Erich Jobst
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -10,33 +10,30 @@
  * Contributors:
  *     Monika Wenger
  *      - initial implementation and rework communication infrastructure
+ *     Martin Jobst
+ *      - add support for infinity and nan values
  *******************************************************************************/
 #include "forte_any_real.h"
 #ifdef FORTE_ENABLE_GENERATED_SOURCE_CPP
 #include "forte_any_real_gen.cpp"
 #endif
 
+#include <cctype>
+
 int CIEC_ANY_REAL::normalizeToStringRepresentation(char *paValue, size_t paBufferSize, int paUsedBytes) const {
-  size_t i = 0;
-  for (; i < paBufferSize; ++i) {
-    if (paValue[i] != '\0') {
-      // Found one
-      if (paValue[i] == '.' || tolower(paValue[i]) == 'e') {
-        return paUsedBytes;
-      }
-    } else {
-      break;
+  // look for decimal point or alphabetic character (indicating exponent, infinity, or nan)
+  for (int i = 0; i < paUsedBytes; ++i) {
+    if (paValue[i] == '.' || std::isalpha(paValue[i])) {
+      return paUsedBytes;
     }
   }
-    // Found none, check if it can be added
+  // found none, check if a decimal point can be added
   if (paUsedBytes + 2 >= static_cast<int>(paBufferSize)) {
     return -1;
-  } else {
-    // Add it
-    paValue[i] = '.';
-    paValue[i + 1] = '0';
-    paValue[i + 2] = '\0';
-    paUsedBytes += 2;
   }
+  // add it
+  paValue[paUsedBytes++] = '.';
+  paValue[paUsedBytes++] = '0';
+  paValue[paUsedBytes] = '\0';
   return paUsedBytes;
 }
