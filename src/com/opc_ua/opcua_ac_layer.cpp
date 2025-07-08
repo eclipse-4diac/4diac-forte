fcc918e9c4e0654d3eaac46c9994172429929707
Markus Meingast <markus.meingast@jku.at>
2025-07-08 13:05:28 +0200

    Update Copyright headers

--- src/com/opc_ua/opcua_ac_layer.cpp
+++ src/com/opc_ua/opcua_ac_layer.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Primetals Technologies Austria GmbH
+ * Copyright (c) 2024, 2025 Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
--- src/com/opc_ua/opcua_local_handler.h
+++ src/com/opc_ua/opcua_local_handler.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2023 Florian Froschermeier <florian.froschermeier@tum.de>,
+ * Copyright (c) 2015, 2025 Florian Froschermeier <florian.froschermeier@tum.de>,
  *                          fortiss GmbH, Primetals Technologies Austria GmbH
  *
  * This program and the accompanying materials are made available under the
