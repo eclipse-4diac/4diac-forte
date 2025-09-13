eee9ad5d85de9fe61cd23fa4a4253e766fdcdc82
Malte Grave <malte.grave@jku.at>
2025-09-13 19:39:40 +0100

    fix: Adapted the CAN layer to the new 4diac-FORTE modular design

---
+++ com/can/CANHandler.cpp
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Alexander Trojnin github.com/trojninalex
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Alexander Trojnin - initial API and implementation and/or initial documentation
+ *  Malte Grave - Adapt the CAN handler to the new 4diac-FORTE structure
+ *******************************************************************************/
+
+#include "forte/cominfra/comCallback.h"
+#include "forte/cominfra/commfb.h"
+#include "forte/devexec.h"
+#include "forte/util/criticalregion.h"
+#include "forte/util/devlog.h"
+
+#include "CANHandler.h"
+
+CCANHandler::CCANHandler(CDeviceExecution &paDeviceExecution) : CExternalEventHandler(paDeviceExecution) {
+  mConnectionListChanged = false;
+}
+
+CCANHandler::~CCANHandler() {
+  mConnectionsList.clear();
+
+  this->end();
+}
+
+void CCANHandler::addComCallback(CFDSelectHandler::TFileDescriptor paFD, forte::com_infra::CComCallback *paCanLayer) {
+  DEVLOG_DEBUG("[CAN Handler] Callback handler added\n");
+
+  CCriticalRegion criticalRegion(mSync);
+  TConnContType stNewNode = {paFD, paCanLayer};
+
+  mConnectionsList.push_back(stNewNode);
+  mConnectionListChanged = true;
+
+  if (!isAlive()) {
+    this->start();
+  }
+}
+
+void CCANHandler::removeComCallback(CFDSelectHandler::TFileDescriptor paFD) {
+  CCriticalRegion criticalRegion(mSync);
+
+  auto itRunner(mConnectionsList.begin());
+  auto itRefNode(mConnectionsList.end());
+  auto itEnd(mConnectionsList.end());
+
+  while (itRunner != itEnd) {
+    if (itRunner->mSockDes == paFD) {
+      if (itRefNode != itEnd) {
+        mConnectionsList.erase(itRefNode);
+      }
+      break;
+    }
+
+    itRefNode = itRunner;
+    ++itRunner;
+  }
+
+  mConnectionListChanged = true;
+}
+
+CFDSelectHandler::TFileDescriptor CCANHandler::createFDSet(fd_set *m_panFDSet) {
+  CFDSelectHandler::TFileDescriptor nRetVal = CFDSelectHandler::scmInvalidFileDescriptor;
+  FD_ZERO(m_panFDSet);
+
+  auto itEnd(mConnectionsList.end());
+  for (auto itRunner = mConnectionsList.begin(); itRunner != itEnd; ++itRunner) {
+    FD_SET(itRunner->mSockDes, m_panFDSet);
+    if (itRunner->mSockDes > nRetVal || CFDSelectHandler::scmInvalidFileDescriptor == nRetVal) {
+      nRetVal = itRunner->mSockDes;
+    }
+  }
+  mConnectionListChanged = false;
+  return nRetVal;
+}
+
+void CCANHandler::run() {
+  while (isAlive()) {
+    auto itEnd(mConnectionsList.end());
+    for (auto itRunner = mConnectionsList.begin(); itRunner != itEnd; ++itRunner) {
+      memset(&frame, 0, sizeof(can_frame));
+
+      if (read(itRunner->mSockDes, &frame, sizeof(struct can_frame)) > 0) {
+        forte::com_infra::CComCallback *called = itRunner->mCalled;
+
+        if (forte::com_infra::e_Nothing != called->recvData(&frame, frame.can_dlc)) {
+          CExternalEventHandler::startNewEventChain(called->getCommFB());
+        }
+      }
+    }
+
+    CThread::sleepThread(1);
+  }
+}
---
+++ com/can/CANHandler.h
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Alexander Trojnin github.com/trojninalex
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Alexander Trojnin - initial API and implementation and/or initial documentation
+ *  Malte Grave - Adapt the CAN handler to the new 4diac-FORTE structure
+ *******************************************************************************/
+
+#ifndef CANHANDLER_H_
+#define CANHANDLER_H_
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include "forte/arch/forte_thread.h"
+#include "forte/arch/sockhand.h"
+#include "forte/extevhan.h"
+
+class CCANHandler : public CExternalEventHandler, public RegisterExternalEventHandler<CCANHandler>, private CThread {
+  public:
+    explicit CCANHandler(CDeviceExecution &paDeviceExecution);
+    ~CCANHandler() override;
+    void addComCallback(CFDSelectHandler::TFileDescriptor paFD, forte::com_infra::CComCallback *paCanLayer);
+    void removeComCallback(CFDSelectHandler::TFileDescriptor paFD);
+
+    void enableHandler() override {
+      start();
+    }
+
+    void disableHandler() override {
+      end();
+    }
+
+    void setPriority(int) {
+    }
+
+    int getPriority() const {
+      return 0;
+    }
+
+  protected:
+    void run() override;
+
+  private:
+    struct TConnContType {
+        CFDSelectHandler::TFileDescriptor mSockDes;
+        forte::com_infra::CComCallback *mCalled;
+    };
+
+    struct can_frame frame;
+
+    typedef std::vector<TConnContType> TConnectionContainer;
+
+    CFDSelectHandler::TFileDescriptor createFDSet(fd_set *m_panFDSet);
+
+    TConnectionContainer mConnectionsList;
+    CSyncObject mSync;
+    bool mConnectionListChanged;
+};
+
+#endif
