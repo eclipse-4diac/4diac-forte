6d15acf151f148ae06733e5c4e94ca34b2d64773
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2026-05-17 12:19:55 +0200

    Add thread deletion policy to CWin32Thread create method
    Incorporates CWin32ThreadDeletePolicy into createThread to ensure proper thread handle management and resource cleanup in Win32 threads.

--- core/arch/win32/src/forte_thread.cpp
+++ core/arch/win32/src/forte_thread.cpp
@@ -25,7 +25,7 @@ namespace forte::arch {
   CWin32Thread::~CWin32Thread() {
   }
 
-  CThreadBase<HANDLE>::TThreadHandleType CWin32Thread::createThread(long paStackSize) {
+  CThreadBase<HANDLE, 0, CWin32ThreadDeletePolicy>::TThreadHandleType CWin32Thread::createThread(long paStackSize) {
     DWORD threadID;
     return CreateThread(0, // default security attributes
                         paStackSize, // use default stack size
