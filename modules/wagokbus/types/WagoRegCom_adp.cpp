bf524b11be7912dd897b21cc50edfd4985e74447
Monika Wenger <monika.wenger@gmx.net>
2026-08-26 18:36:41 +0200

    add copyright headers

--- modules/wagokbus/types/WagoRegCom_adp.cpp
+++ modules/wagokbus/types/WagoRegCom_adp.cpp
@@ -1,4 +1,11 @@
 /*************************************************************************
+ * Copyright (c) 2026 Monika Wenger
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
  *** FORTE Library Element
  ***
  *** This file was generated using the 4DIAC FORTE Export Filter 3.2.100.qualifier!
